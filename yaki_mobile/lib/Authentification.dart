cdbba260b1838135406cf746677d6d3a07432dc5
HAVEZ Maxime <havezmaxime@mbp-de-havez.home>
2023-02-21 15:12:32 +0100

    feat(Generic_Component): Header component as generic

--- yaki_mobile/lib/Authentification.dart
+++ yaki_mobile/lib/Authentification.dart
@@ -2,23 +2,29 @@ import 'package:flutter/material.dart';
 import 'presentation/ui/shared/views/Header.dart';
 
 class Authentification extends StatelessWidget {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     // return the size of the device
-    var size = MediaQuery.of(context).size;
 
     return Scaffold(
         body: Column(
-      children: [
-        Expanded(
-            flex: 6,
-            child: Header(
-              pictoIcon: 'assets/images/dots.svg',
-              pictoPath: 'assets/images/authent.svg',
-              headerTitle: 'Authentification',
-              headerHint: 'Who are you ?',
-            )),
-        const Expanded(flex: 7, child: Text(' '))
+          children: [
+            Expanded(
+                flex: 6,
+                child: Header(
+                  pictoIcon: 'assets/images/dots.svg',
+                  pictoPath: 'assets/images/authent.svg',
+                  headerTitle: 'Authentification',
+                  headerHint: 'Who are you ?',
+                )),
+             Expanded(
+                flex: 7,
+                child: Container(
+
+                ))
       ],
     ));
   }
