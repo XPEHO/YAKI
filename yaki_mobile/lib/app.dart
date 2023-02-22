1dbd0c7ead0711963acac55c3a8f657a8f19877c
Maxime HAVEZ <75071752+MaximeHavez@users.noreply.github.com>
2023-02-22 11:29:35 +0100

    feat/genericheader (#29)
    * feat(genericheader): make localization
    
    * feat(genericheader): Generic header for the application
    
    * feat(genericheader): Generic header for the application
    
    ---------
    
    Co-authored-by: HAVEZ Maxime <havezmaxime@mbp-de-havez.home>

---
+++ yaki_mobile/lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'appRouter.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp.router(
+      locale: context.locale,
+      supportedLocales: context.supportedLocales,
+      localizationsDelegates: context.localizationDelegates,
+      title: 'Yaki',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routerConfig: appRouter, // Referencing navigation router
+    );
+
+  }
+
+}
---
+++ yaki_mobile/lib/presentation/styles/HeaderTextStyle.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class HeaderColor {
+  static const Color yellowApp = Color.fromARGB(255, 245, 205, 61);
+}
+
+TextStyle textStyleTemp() {
+  return const TextStyle(
+      fontSize: 23,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+  );
+}
