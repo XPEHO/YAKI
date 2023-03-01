ef27705960188d18e297a66f2074c25b886be0e6
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-03-01 09:13:09 +0100

    feat(authentication): removed dead code

--- yaki_backend/src/features/user/authentication.service.ts
+++ yaki_backend/src/features/user/authentication.service.ts
@@ -8,28 +8,6 @@ var jwt = require('jsonwebtoken');
 
 class Service {
     /**
-     * Config details for hash algorithm 
-     */
-    __hashingConfig = { // based on OWASP cheat sheet recommendations (as of March, 2022)
-        parallelism: 1,
-        memoryCost: 64000, // 64 mb
-        timeCost: 5 // number of itetations
-    }
-
-    /**
-     * Hash password with a salt
-     * @param password 
-     * @returns 
-     */
-    hashPassword = async(password: string) => {
-        let salt = crypto.randomBytes(16);
-        return await argon2.hash(password, {
-            ...this.__hashingConfig,
-            salt,
-        })
-    }
-
-    /**
      * Check if the password send by the client match the one from the database
      * @param password 
      * @param hash 
