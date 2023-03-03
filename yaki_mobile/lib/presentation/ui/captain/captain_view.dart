ba317aa642bdf6bf1a6114c9aa75681b2732ed44
havezmaxime <havez.maxime@gmail.com>
2023-03-03 16:50:38 +0100

    feat(captainView): rebasing

---
+++ yaki_mobile/lib/presentation/ui/captain/captain_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:yaki/presentation/ui/captain/views/captain_body.dart';
+import 'package:yaki/presentation/ui/shared/pages_layout.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:yaki/presentation/ui/shared/views/header.dart';
+
+class CaptainView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CaptainView();
+
+}
+
+class _CaptainView extends State<CaptainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PagesLayout(
+        header: Header(
+          pictoIcon: 'assets/images/avatar1.svg',
+          pictoPath: 'assets/images/unknown.svg',
+          headerTitle: tr('headerTitleDecla'),
+          headerHint: tr('headerHintDecla'),
+        ),
+          bodyContent: CaptainBody(),
+      ),
+    );
+  }
+  
+}
