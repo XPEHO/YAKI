fbd4f5b440e435e7ad3e2eeea832315f7f827a32
HaddoucheRym <107027392+HaddoucheRym@users.noreply.github.com>
2023-03-06 11:53:54 +0100

    Feat/status back (#56)
    * chore(workflows): fix workflows for dependabot
    
    * chore(deps-dev): bump @types/node in /yaki_backend (#50)
    
    Bumps [@types/node](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/HEAD/types/node) from 18.13.0 to 18.14.5.
    - [Release notes](https://github.com/DefinitelyTyped/DefinitelyTyped/releases)
    - [Commits](https://github.com/DefinitelyTyped/DefinitelyTyped/commits/HEAD/types/node)
    
    ---
    updated-dependencies:
    - dependency-name: "@types/node"
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps): bump flutter_riverpod from 2.2.0 to 2.3.0 in /yaki_mobile (#54)
    
    Bumps [flutter_riverpod](https://github.com/rrousselGit/riverpod) from 2.2.0 to 2.3.0.
    - [Release notes](https://github.com/rrousselGit/riverpod/releases)
    - [Commits](https://github.com/rrousselGit/riverpod/compare/flutter_riverpod-v2.2.0...flutter_riverpod-v2.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: flutter_riverpod
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps): bump golden_toolkit from 0.14.0 to 0.15.0 in /yaki_mobile (#53)
    
    Bumps [golden_toolkit](https://github.com/eBay/flutter_glove_box/tree/master/packages) from 0.14.0 to 0.15.0.
    - [Release notes](https://github.com/eBay/flutter_glove_box/releases)
    - [Commits](https://github.com/eBay/flutter_glove_box/commits/HEAD/packages)
    
    ---
    updated-dependencies:
    - dependency-name: golden_toolkit
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps-dev): bump nodemon from 2.0.20 to 2.0.21 in /yaki_backend (#48)
    
    Bumps [nodemon](https://github.com/remy/nodemon) from 2.0.20 to 2.0.21.
    - [Release notes](https://github.com/remy/nodemon/releases)
    - [Commits](https://github.com/remy/nodemon/compare/v2.0.20...v2.0.21)
    
    ---
    updated-dependencies:
    - dependency-name: nodemon
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps): bump swagger-ui-express in /yaki_backend (#49)
    
    Bumps [swagger-ui-express](https://github.com/scottie1984/swagger-ui-express) from 4.6.0 to 4.6.2.
    - [Release notes](https://github.com/scottie1984/swagger-ui-express/releases)
    - [Commits](https://github.com/scottie1984/swagger-ui-express/compare/4.6.0...4.6.2)
    
    ---
    updated-dependencies:
    - dependency-name: swagger-ui-express
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * feat(status-back): implemented get and update status methods
    
    * feat(status-back): modified readme
    
    * feat(status-back): added token
    
    * feat(status-back): renamed declarationcontroller file
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: PiotrFLEURY <piotr.fleury@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ yaki_backend/src/features/declaration/declaration.controller.ts
@@ -0,0 +1,63 @@
+import { Declaration } from "./declaration.interface";
+import { DeclarationService } from "./declaration.service";
+import { Request, Response } from 'express';
+
+
+export class DeclarationController {
+    private declarationService: DeclarationService;
+
+    /**
+     * Creates a new instance of DeclarationController.
+     * @param declarationService The DeclarationService instance to be used by this controller.
+     */
+    constructor(declarationService: DeclarationService) {
+        this.declarationService = declarationService;
+    }
+
+    /**
+     * Handles an incoming HTTP POST request to create a new declaration.
+     * @param req The incoming HTTP request.
+     * @param res The HTTP response to be sent.
+     * @returns A promise that returns nothing.
+     */
+    async createDeclaration(req: Request, res: Response): Promise<void> {
+        const declaration: Declaration = req.body;
+        try {
+            const createdDeclaration = await this.declarationService.createDeclaration(declaration);
+            res.status(201).json(createdDeclaration);
+        } catch (error: any) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+    /**
+     * It gets the declarations for a team mate.
+     * @param {Request} req - Request - the request object
+     * @param {Response} res - Response - the response object
+     */
+    async getDeclarationsForTeamMate(req: Request, res: Response) {
+        const teamMateId = parseInt(req.params.teamMateId);
+        try {
+            const declarations = await this.declarationService.getDeclarationsForTeamMate(teamMateId);
+            res.status(200).json(declarations);
+        } catch (error: any) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+    /**
+     * Updating the declaration status.
+     * @param req The incoming HTTP request.
+     * @param res The HTTP response to be sent.
+     */
+    async updateDeclarationStatus(req: Request, res: Response) {
+        const declarationId = parseInt(req.params.declarationId);
+        const declaration: Declaration = req.body;
+        try {
+            await this.declarationService.updateDeclarationStatus(declarationId, declaration);
+            res.status(200).json(declaration);
+        } catch (error: any) {
+            res.status(500).json({ message: error.message });
+        }
+    };
+}
