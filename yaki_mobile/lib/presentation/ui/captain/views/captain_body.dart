69575d0d020ace49a3858cd0c7b23f8ae818ff99
havezmaxime <havez.maxime@gmail.com>
2023-03-06 15:26:15 +0100

    feat(captainView): card captain view

--- yaki_mobile/lib/presentation/ui/captain/views/captain_body.dart
+++ yaki_mobile/lib/presentation/ui/captain/views/captain_body.dart
@@ -1,28 +1,45 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yaki/presentation/ui/shared/views/Header.dart';
 import 'package:yaki/presentation/ui/shared/views/InputApp.dart';
 
 class CaptainBody extends ConsumerWidget {
-
   final captainInputController = TextEditingController();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var size = MediaQuery.of(context).size;
+
     return Column(
       children: [
         Padding(
           padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
           child: InputApp(
-              inputText: tr('inputCaptain'),
-              inputHint: tr('hintCaptain'),
-              password: false,
-              controller: captainInputController,
+            inputText: tr('inputCaptain'),
+            inputHint: tr('hintCaptain'),
+            password: false,
+            controller: captainInputController,
+          ),
+        ),
+        SizedBox(
+          width: size.width * 0.95,
+          height: size.height * 0.15,
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[
+                CircleAvatarSVG(
+                    iconPath: 'assets/images/captain.svg',
+                    radius: 50,
+                ),
+              ],
+            ),
           ),
         ),
       ],
     );
   }
-
 }
