e2917e0fef69db31a7cee5334ae405e884f3ed1f
laurent <laurentmag@Macbook-Xpeho-Laurent.lan>
2023-03-07 18:44:15 +0100

    feat(status): working interceptor to send token with request

--- yaki_mobile/lib/presentation/state/providers/dio_provider.dart
+++ yaki_mobile/lib/presentation/state/providers/dio_provider.dart
@@ -5,20 +5,26 @@ import 'package:shared_preferences/shared_preferences.dart';
 final dioProviderInterceptor = Provider((ref) {
   // use dio instance to setup interceptors, which allow to use dynamic data
   final dio = Dio();
+  final customInterceptor = AddTokenToHeader();
   dio.interceptors.add(
     InterceptorsWrapper(
-      onRequest: (RequestOptions options, handler) => requestInterceptorTest(options),
+      onRequest: (RequestOptions options, handler) =>
+          customInterceptor.onRequest(options, handler),
     ),
   );
   return dio;
 });
 
+class AddTokenToHeader extends Interceptor {
+  @override
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var token = prefs.get("token");
 
-dynamic requestInterceptorTest(RequestOptions options) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  var token = prefs.get("token");
+    options.headers.addAll({"x-access-token": token});
+    //print('REQUEST[${options.method}] => PATH: ${options.path}');
 
-  options.headers.addAll({"x-access-token": token});
-
-  return options;
+    return super.onRequest(options, handler);
+  }
 }
