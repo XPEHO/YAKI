33c1ef36d3fdf63c897e57ca5201852753fea7b7
laurent <laurentmag@Macbook-Xpeho-Laurent.lan>
2023-03-10 15:33:22 +0100

    refactor(status): declaration repo statusvalue creation

--- yaki_mobile/lib/data/repositories/declaration_respository.dart
+++ yaki_mobile/lib/data/repositories/declaration_respository.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:retrofit/dio.dart';
 import 'package:yaki/data/sources/remote/declaration_api.dart';
 import 'package:yaki/data/models/declaration_model.dart';
 import 'package:yaki/domain/entities/declaration_status.dart';
 
 class DeclarationRepository {
   final DeclarationApi _declarationApi;
+  DeclarationStatus? declarationStatus;
 
   DeclarationRepository(this._declarationApi);
-
-  DeclarationStatus? declarationStatus;
 
   Future<int?> create(DeclarationModel declaration) async {
     final httpResponse = await _declarationApi.create(declaration);
     final statusCode = httpResponse.response.statusCode;
 
+    String statusValue = generateStatusValue(statusCode, httpResponse.data.declaration_status);
+    declarationStatus = DeclarationStatus(
+      status: statusValue,
+    );
+    return statusCode;
+  }
+
+  String generateStatusValue(int? statusCode, String httpResponseStatus) {
     String statusValue = "";
     try {
       if ([200, 201].contains(statusCode)) {
-        statusValue = httpResponse.data.declaration_status;
+        statusValue = httpResponseStatus;
       } else if ([400, 500].contains(statusCode)) {
         statusValue = "nop";
         debugPrint("code error : $statusCode");
@@ -26,12 +34,7 @@ class DeclarationRepository {
       statusValue = "nop";
       debugPrint("error during creation $exception");
     }
-
-    declarationStatus = DeclarationStatus(
-      status: statusValue,
-    );
-
-    return statusCode;
+    return statusValue;
   }
 
   String get status {
