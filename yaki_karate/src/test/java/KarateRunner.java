607350baf666a5b2e80313a6f7f446767521e703
PiotrFLEURY <piotr.fleury@gmail.com>
2023-03-16 17:49:09 +0100

    chore(karate): setup karate testing

---
+++ yaki_karate/src/test/java/KarateRunner.java
@@ -0,0 +1,10 @@
+import com.intuit.karate.junit5.Karate;
+
+public class KarateRunner {
+
+    @Karate.Test
+    Karate runAllTests() {
+        return Karate.run().relativeTo(getClass());
+    }
+
+}
