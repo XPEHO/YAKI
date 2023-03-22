33b2bbca64f691839a12d3bd90e1f122e82d5bd2
SamirD <samir.decambray@gmail.com>
2023-03-22 17:25:48 +0100

    test(karate):create karate test done

--- yaki_backend/src/features/declaration/declaration.controller.ts
+++ yaki_backend/src/features/declaration/declaration.controller.ts
@@ -1,84 +1,85 @@
-import { DeclarationService } from "./declaration.service";
+import { DeclarationService } from './declaration.service';
 import { Request, Response } from 'express';
-import { DeclarationDtoIn } from "./declaration.dtoIn";
-
-
-
+import { DeclarationDtoIn } from './declaration.dtoIn';
 
 export class DeclarationController {
-    private declarationService: DeclarationService;
+  private declarationService: DeclarationService;
 
-    /**
-     * Creates a new instance of DeclarationController.
-     * @param declarationService The DeclarationService instance to be used by this controller.
-     */
-    constructor(declarationService: DeclarationService) {
-        this.declarationService = declarationService;
+  /**
+   * Creates a new instance of DeclarationController.
+   * @param declarationService The DeclarationService instance to be used by this controller.
+   */
+  constructor(declarationService: DeclarationService) {
+    this.declarationService = declarationService;
+  }
+
+  /**
+   * Handles an incoming HTTP POST request to create a new declaration.
+   * @param req The incoming HTTP request.
+   * @param res The HTTP response to be sent.
+   * @returns A promise that returns nothing.
+   */
+  async createDeclaration(req: Request, res: Response): Promise<void> {
+    const declaration: DeclarationDtoIn = req.body;
+    try {
+      const createdDeclaration =
+        await this.declarationService.createDeclaration(declaration);
+      res.status(201).json(createdDeclaration);
+    } catch (error: any) {
+      if (error instanceof TypeError) {
+        // catch bad request errors
+        res.status(400).json({ message: error.message });
+      } else {
+        // catch server errors
+        res.status(500).json({ message: error.message });
+      }
     }
+  }
 
-    /**
-     * Handles an incoming HTTP POST request to create a new declaration.
-     * @param req The incoming HTTP request.
-     * @param res The HTTP response to be sent.
-     * @returns A promise that returns nothing.
-     */
-    async createDeclaration(req: Request, res: Response): Promise<void> {
-        const declaration: DeclarationDtoIn = req.body;
-        try {
-            const createdDeclaration = await this.declarationService.createDeclaration(declaration);
-            res.status(201).json(createdDeclaration);
-        } catch (error: any) {
-
-            if (error instanceof TypeError) {
-                // catch bad request errors 
-                res.status(400).json({ message: error.message });
-            } else {
-                // catch server errors 
-                res.status(500).json({ message: error.message });
-            }
-        }
+  /**
+   * It gets the declaration for a team mate.
+   * @param {Request} req - Request - the request object
+   * @param {Response} res - Response - the response object
+   */
+  async getDeclarationsForTeamMate(req: Request, res: Response) {
+    const teamMateId = Number(req.query.teamMateId);
+    try {
+      const declarations =
+        await this.declarationService.getDeclarationForTeamMate(teamMateId);
+      res.status(200).json(declarations);
+    } catch (error: any) {
+      if (error instanceof TypeError) {
+        // catch not found errors
+        res.status(404).json({ message: error.message });
+      } else {
+        // catch server errors
+        res.status(500).json({ message: error.message });
+      }
     }
+  }
 
-    /**
-     * It gets the declaration for a team mate.
-     * @param {Request} req - Request - the request object
-     * @param {Response} res - Response - the response object
-     */
-    async getDeclarationsForTeamMate(req: Request, res: Response) {
-        const teamMateId = Number(req.query.teamMateId);
-        try {
-            const declarations = await this.declarationService.getDeclarationForTeamMate(teamMateId);
-            res.status(200).json(declarations);
-        } catch (error: any) {
-            if (error instanceof TypeError) {
-                // catch not found errors
-                res.status(404).json({ message: error.message });
-            } else {
-                // catch server errors 
-                res.status(500).json({ message: error.message });
-            }
-        }
+  /**
+   * Updating the declaration status.
+   * @param req The incoming HTTP request.
+   * @param res The HTTP response to be sent.
+   */
+  async updateDeclarationStatus(req: Request, res: Response) {
+    const declarationId = parseInt(req.params.declarationId);
+    const declaration: DeclarationDtoIn = req.body;
+    try {
+      await this.declarationService.updateDeclarationStatus(
+        declarationId,
+        declaration
+      );
+      res.status(200).json(declaration);
+    } catch (error: any) {
+      if (error instanceof TypeError) {
+        // catch not found errors
+        res.status(404).json({ message: error.message });
+      } else {
+        // catch server errors
+        res.status(500).json({ message: error.message });
+      }
     }
-
-    /**
-     * Updating the declaration status.
-     * @param req The incoming HTTP request.
-     * @param res The HTTP response to be sent.
-     */
-    async updateDeclarationStatus(req: Request, res: Response) {
-        const declarationId = parseInt(req.params.declarationId);
-        const declaration: DeclarationDtoIn = req.body;
-        try {
-            await this.declarationService.updateDeclarationStatus(declarationId, declaration);
-            res.status(200).json(declaration);
-        } catch (error: any) {
-            if (error instanceof TypeError) {
-                // catch not found errors
-                res.status(404).json({ message: error.message });
-            } else {
-                // catch server errors 
-                res.status(500).json({ message: error.message });
-            }
-        }
-    };
+  }
 }
