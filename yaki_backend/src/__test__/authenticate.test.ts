7d4e6ffbb34336e1e5f575e6a8f1081af4dc0411
Rym <rymmihoub@gmail.com>
2023-03-22 15:04:21 +0100

    test(login_jest): login jest test passed

--- yaki_backend/src/__test__/authenticate.test.ts
+++ yaki_backend/src/__test__/authenticate.test.ts
@@ -28,34 +28,5 @@ describe('post /login', () => {
 
 
 
-  // describe('get /captains', () => {
-  //   test('Should get captains', async () => {
-  //     await request(app)
-  //       .get('/captains')
-  //       .send({
-  //         login: 'lavigne',
-  //         password: 'lavigne',
-  //       })
-  //       .expect(200);
-  //   });
-  //   test('Should not get captains', async () => {
-  //     await request(app)
-  //       .get('/captains')
-  //       .send({
-  //         login: 'lavigne',
-  //         password: 'wrongPassword',
-  //       })
-  //       .expect(204);
-  //   });
-  // });
 
-  // describe('get /teamMates', () => {
-  //   test('Should get teammates', async () => {
-  //     await request(app).get('/teamMates').expect(200);
-  //   });
-  //   test('Should not get teammates', () => {
-  //     request(app).get('/teamMate').expect(204);
-  //   });
-  // });
-// });
 
