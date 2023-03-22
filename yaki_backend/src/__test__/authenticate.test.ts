43fef6c1ee9256f6e5dcbc7a5105c4c0960f3702
Rym <rymmihoub@gmail.com>
2023-03-22 15:00:54 +0100

    test(login_jest): login jest test passed

--- yaki_backend/src/__test__/authenticate.test.ts
+++ yaki_backend/src/__test__/authenticate.test.ts
@@ -1,38 +1,26 @@
-// const request = require('supertest');
-// const app = 'http://localhost:3000';
-
-// describe('post /login', () => {
-//   test('Should login user', async () => {
-//     await request(app)
-//       .post('/login')
-//       .send({
-//         login: 'dugrand',
-//         password: 'dugrand',
-//       })
-//       .expect(200);
-//   });
-
-//   test('Should fail login user', async () => {
-//     await request(app)
-//       .post('/login')
-//       .send({
-//         login: 'dugrand',
-//         password: 'duGrand',
-//       })
-//       .expect(204);
-//   });
-
-  import { login } from '../features/user/authentication.service';
-
-describe('login', () => {
-  test('returns true when given correct username and password', () => {
-    expect(login('example', 'password')).toBe(true);
+const request = require('supertest');
+const app = 'http://localhost:3000';
+describe('post /login', () => {
+  test('Should login user', async () => {
+    await request(app)
+      .post('/login')
+      .send({
+        login: 'dugrand',
+        password: 'dugrand',
+      })
+      .expect(200);
   });
-
-  test('returns false when given incorrect username and password', () => {
-    expect(login('wrong', 'credentials')).toBe(false);
+  test('Should fail login user', async () => {
+    await request(app)
+      .post('/login')
+      .send({
+        login: 'dugrand',
+        password: 'duGrand',
+      })
+      .expect(204);
   });
 });
+
 
 
 
@@ -70,3 +58,4 @@ describe('login', () => {
   //   });
   // });
 // });
+
