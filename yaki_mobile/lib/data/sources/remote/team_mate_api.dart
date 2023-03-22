13b183bc4d1f8f2c0913850de497efeaecfd39c6
laurent <laurentmag@Macbook-Xpeho-Laurent.lan>
2023-03-22 12:01:27 +0100

    refactor(httpResponse): remove httpResponse type, repo refactored according, remove unused api

--- yaki_mobile/lib/data/sources/remote/team_mate_api.dart
+++ yaki_mobile/lib/data/sources/remote/team_mate_api.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
-import 'package:yaki/data/models/team_mate_model.dart';
 
 part 'team_mate_api.g.dart';
 
@@ -9,5 +8,5 @@ abstract class TeamMateApi {
   factory TeamMateApi(Dio dio, {required String baseUrl}) = _TeamMateApi;
 
   @GET('/teamMates')
-  Future<List<TeamMateModel>> getTeamMate();
+  Future<HttpResponse> getTeamMate();
 }
