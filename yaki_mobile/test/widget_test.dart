22fc37be90c972eb0079c64482338c4ed06a8a4d
PiotrFLEURY <piotr.fleury@gmail.com>
2023-03-23 17:09:43 +0100

    test(golden): create golden tests

--- yaki_mobile/test/widget_test.dart
+++ yaki_mobile/test/widget_test.dart
@@ -5,15 +5,58 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:yaki/data/sources/remote/declaration_api.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:yaki/presentation/ui/shared/views/header.dart';
 
-//import 'widget_test.mocks.dart';
-
-@GenerateMocks([DeclarationApi])
 void main() {
-  test('super test', () {
-    expect(true, true);
+  EasyLocalization.logger.enableLevels = [];
+
+  /// Golden Test
+  ///
+  /// This test verifies the integrity of the Header widget
+  /// and all its occurrences
+  testGoldens('Header must look correct', (tester) async {
+    final builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(
+        devices: [Device.iphone11, Device.phone],
+      )
+      ..addScenario(
+        /// Header Captain
+        name: 'Captain',
+        widget: Header(
+          pictoIcon: 'assets/images/captain.svg',
+          pictoPath: 'assets/images/captain.svg',
+          headerTitle: tr('captainTitle'),
+          headerHint: tr('captainHeaderTitle'),
+        ),
+      )
+      ..addScenario(
+        /// Header Declaration
+        name: 'Declaration',
+        widget: Header(
+          pictoIcon: 'assets/images/avatar1.svg',
+          pictoPath: 'assets/images/unknown.svg',
+          headerTitle: tr('headerTitleDecla'),
+          headerHint: tr('headerHintDecla'),
+        ),
+      )
+      ..addScenario(
+        /// Header Authentication
+        name: 'Authentication',
+        widget: Header(
+          pictoIcon: 'assets/images/dots.svg',
+          pictoPath: 'assets/images/authent.svg',
+          headerTitle: tr('headerTitle'),
+          headerHint: tr('headerHint'),
+        ),
+      );
+
+    /// read the widget
+    await tester.pumpDeviceBuilder(builder);
+
+    /// Compare the widget with the registered picture
+    /// Create the reference picture with command line : "flutter test --update-goldens"
+    await screenMatchesGolden(tester, 'Header');
   });
 }
