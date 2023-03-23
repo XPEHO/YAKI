81bbb6b70d21fa9d8efc6ff7e20a75511c2d2c8f
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-03-23 17:31:30 +0100

    test(user-login): add mockup data and beginning of user service testing

--- yaki_backend/src/__test__/user-service.test.ts
+++ yaki_backend/src/__test__/user-service.test.ts
@@ -1,16 +1,17 @@
 import { UserService } from "../features/user/user.service";
 import { UserRepository } from "../features/user/user.repository";
-import mockDb from "./__mocks__/user";
+import dbMockup from "./__mocks__/user";
 import UserModel from "../features/user/user.dtoIn";
-import { TeamMateDtoOut } from "../features/teamMate/teamMate.dtoOut";
-import { CaptainDtoOut } from "../features/captain/captain.dtoOut";
 
 jest.mock('../features/user/user.repository', () => {
     return {
         UserRepository: jest.fn().mockImplementation(() => {
             return {
                 getByLogin: (login: string) : UserModel => {
-                    const user = mockDb.filter(elm => elm.user_login == login);
+                    const user = dbMockup.filter(elm => elm.user_login == login);
+                    if(user == null) {
+                        throw new Error('Bad authentification details');
+                    }
                     return user[0];
                 }
             }
