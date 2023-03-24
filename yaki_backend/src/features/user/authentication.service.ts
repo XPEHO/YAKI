aded3db1d66d493c75b203efce521b221e1ff645
Rym <rymmihoub@gmail.com>
2023-03-24 16:02:02 +0100

    test(login_jest): successful unit tests jest of authetificateService

--- yaki_backend/src/features/user/authentication.service.ts
+++ yaki_backend/src/features/user/authentication.service.ts
@@ -30,10 +30,13 @@ class Service {
   createToken = async (user: CaptainDtoOut | TeamMateDtoOut) => {
     console.log( process.env.TOKEN_SECRET);
     
+<<<<<<< HEAD
 >>>>>>> e84c1fc (test(login_jest): successful unit tests jest of authetificateService)
 =======
   createToken = async (user: CaptainDtoOut | TeamMateDtoOut) => {  
 >>>>>>> 0dceb75 (test(login_jest): deleted log from authenticationService)
+=======
+>>>>>>> e961e35 (test(login_jest): successful unit tests jest of authetificateService)
     const token = jwt.sign(
       {
         user_id: user.userId,
