376ab54f31f2e5457e15b68bbcbe2a97b03b1f24
profotoce59 <romain.vanhee@gmail.com>
2023-03-24 15:51:58 +0100

    feat(captain_get): get all captain method implemented

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/models/UserModel.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/models/UserModel.java
@@ -3,32 +3,83 @@ package com.xpeho.yaki_admin_backend.data.models;
 import jakarta.persistence.*;
 
 
-@Inheritance(strategy= InheritanceType.JOINED)
-@Table(name = "user")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "user", schema = "public")
 @Entity
 public abstract class UserModel {
+    
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
-    int id;
+    private int userId;
     @Column(name = "user_last_name")
-    String lastName;
+    private String lastName;
     @Column(name = "user_first_name")
-    String firstName;
+    private String firstName;
     @Column(name = "user_email")
-    String email;
+    private String email;
     @Column(name = "user_login")
-    String login;
+    private String login;
     @Column(name = "user_password")
-    String password;
+    private String password;
 
-    public UserModel(int id, String lastName, String firstName, String email, String login, String password) {
-        this.id = id;
+    public UserModel(int userId, String lastName, String firstName, String email, String login, String password) {
+        this.userId = userId;
         this.lastName = lastName;
         this.firstName = firstName;
         this.email = email;
         this.login = login;
         this.password = password;
     }
+
     public UserModel() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
