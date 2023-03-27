7e89b9019e6fe54293b0aed7f60a8e0b417c0a72
Rym <rymmihoub@gmail.com>
2023-03-27 16:48:57 +0200

    test(declaration_jest): unit test create declaration

--- yaki_backend/src/__test__/declaration-service.test.ts
+++ yaki_backend/src/__test__/declaration-service.test.ts
@@ -1,4 +1,42 @@
 import { DeclarationService } from '../features/declaration/declaration.service';
 import { DeclarationDtoIn } from '../features/declaration/declaration.dtoIn';
+import { StatusDeclaration } from '../features/declaration/status.enum';
+import { DeclarationRepository } from "../features/declaration/declaration.repository";
 
+describe("DeclarationService", () => {
+    let declarationService: DeclarationService;
+    let declarationRepository: DeclarationRepository;
 
+    beforeEach(() => {
+        declarationRepository = new DeclarationRepository();
+        declarationService = new DeclarationService(declarationRepository);
+    });
+    describe('create a declaration ', () => {
+        const declarationDtoIn: DeclarationDtoIn = new DeclarationDtoIn(
+            1,
+            1,
+            new Date(),
+            StatusDeclaration.REMOTE
+        )
+
+        it('should create and return a new declaration', async () => {
+            jest.spyOn(declarationRepository, "createDeclaration").mockResolvedValueOnce(declarationDtoIn);
+
+            const createdDeclaration = await declarationService.createDeclaration(declarationDtoIn);
+
+            expect(createdDeclaration).toEqual(declarationDtoIn);
+        })
+    
+
+        it("should throw a TypeError if mandatory information is missing", async () => {
+          const declaration = {
+            declarationId: 1,
+            declarationTeamMateId: '123',
+            declarationDate: new Date,
+            declarationStatus: '',
+          };
+          await expect(declarationService.createDeclaration(declaration)).rejects.toThrow(TypeError);
+          expect(declarationRepository.createDeclaration).not.toHaveBeenCalled();
+        })
+})
+})
