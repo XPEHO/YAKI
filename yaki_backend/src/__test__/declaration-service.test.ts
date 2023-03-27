cbb83a94e886cee06645896b01767b755fdb7b6c
Rym <rymmihoub@gmail.com>
2023-03-27 17:16:24 +0200

    test(declaration_jest): unit test for create declaration

--- yaki_backend/src/__test__/declaration-service.test.ts
+++ yaki_backend/src/__test__/declaration-service.test.ts
@@ -4,39 +4,37 @@ import { StatusDeclaration } from '../features/declaration/status.enum';
 import { DeclarationRepository } from "../features/declaration/declaration.repository";
 
 describe("DeclarationService", () => {
-    let declarationService: DeclarationService;
-    let declarationRepository: DeclarationRepository;
+  let declarationService: DeclarationService;
+  let declarationRepository: DeclarationRepository;
 
-    beforeEach(() => {
-        declarationRepository = new DeclarationRepository();
-        declarationService = new DeclarationService(declarationRepository);
-    });
-    describe('create a declaration ', () => {
-        const declarationDtoIn: DeclarationDtoIn = new DeclarationDtoIn(
-            1,
-            1,
-            new Date(),
-            StatusDeclaration.REMOTE
-        )
+  beforeEach(() => {
+    declarationRepository = new DeclarationRepository();
+    declarationService = new DeclarationService(declarationRepository);
+  });
+  describe('create a declaration ', () => {
+    const declarationDtoIn: DeclarationDtoIn = new DeclarationDtoIn(
+      1,
+      1,
+      new Date(),
+      StatusDeclaration.REMOTE
+    )
 
-        it('should create and return a new declaration', async () => {
-            jest.spyOn(declarationRepository, "createDeclaration").mockResolvedValueOnce(declarationDtoIn);
+    it('should create and return a new declaration', async () => {
+      jest.spyOn(declarationRepository, "createDeclaration").mockResolvedValueOnce(declarationDtoIn);
 
-            const createdDeclaration = await declarationService.createDeclaration(declarationDtoIn);
+      const createdDeclaration = await declarationService.createDeclaration(declarationDtoIn);
 
-            expect(createdDeclaration).toEqual(declarationDtoIn);
-        })
-    
+      expect(createdDeclaration).toEqual(declarationDtoIn);
+    })
 
-        it("should throw a TypeError if mandatory information is missing", async () => {
-          const declaration = {
-            declarationId: 1,
-            declarationTeamMateId: '123',
-            declarationDate: new Date,
-            declarationStatus: '',
-          };
-          await expect(declarationService.createDeclaration(declaration)).rejects.toThrow(TypeError);
-          expect(declarationRepository.createDeclaration).not.toHaveBeenCalled();
-        })
-})
+    it("should throw a TypeError if mandatory information is missing", async () => {
+      const declaration: any = {
+        declarationId: 1,
+        declarationTeamMateId: undefined,
+        declarationDate: new Date,
+        declarationStatus: '',
+      };
+      await expect(declarationService.createDeclaration(declaration)).rejects.toThrow(TypeError);
+    })
+  })
 })
