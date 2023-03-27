1980d84bfb3da80f8629f40983df4a6261485b0b
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-03-27 11:03:42 +0200

    test(user-auth): add test for team mate and captain creation methods

--- yaki_backend/src/__test__/user-service.test.ts
+++ yaki_backend/src/__test__/user-service.test.ts
@@ -1,27 +1,24 @@
-test("it should pass", async () => {
-    expect(true).toBe(true);
-  });
+import { UserService } from "../features/user/user.service";
+import { UserRepository } from "../features/user/user.repository";
+import mockDb from "./__mocks__/user";
+import UserModel from "../features/user/user.dtoIn";
+import { TeamMateDtoOut } from "../features/teamMate/teamMate.dtoOut";
+import { CaptainDtoOut } from "../features/captain/captain.dtoOut";
 
-// import { UserService } from "../features/user/user.service";
-// import { UserRepository } from "../features/user/user.repository";
-// import dbMockup from "./__mocks__/user";
-// import UserModel from "../features/user/user.dtoIn";
+jest.mock('../features/user/user.repository', () => {
+    return {
+        UserRepository: jest.fn().mockImplementation(() => {
+            return {
+                getByLogin: (login: string) : UserModel => {
+                    const user = mockDb.filter(elm => elm.user_login == login);
+                    return user[0];
+                }
+            }
+        })
+    }
+});
 
-// jest.mock('../features/user/user.repository', () => {
-//     return {
-//         UserRepository: jest.fn().mockImplementation(() => {
-//             return {
-//                 getByLogin: (login: string) : UserModel => {
-//                     const user = dbMockup.filter(elm => elm.user_login == login);
-//                     if(user == null) {
-//                         throw new Error('Bad authentification details');
-//                     }
-//                     return user[0];
-//                 }
-//             }
-//         })
-//     }
-// });
+const mockedUserRepo = jest.mocked(UserRepository, {shallow: true});
 
 describe('check login details', () => {
     const useRepo = new UserRepository();
