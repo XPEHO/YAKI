3bacd4a4550d31b17e6fb816055c36d573ec875d
HaddoucheRym <107027392+HaddoucheRym@users.noreply.github.com>
2023-03-28 17:07:48 +0200

    test(updateDeclaration_jext) (#166)
    * test(updateDeclaration_jext): modified declarationService and unit test updateDeclaration successful
    
    * test(updateDeclaration_jest): modified karate test

--- yaki_backend/src/features/declaration/declaration.service.ts
+++ yaki_backend/src/features/declaration/declaration.service.ts
@@ -1,5 +1,6 @@
-import {DeclarationRepository} from "./declaration.repository";
-import {DeclarationDtoIn} from "./declaration.dtoIn";
+import { DeclarationRepository } from "./declaration.repository";
+import { DeclarationDtoIn } from "./declaration.dtoIn";
+import { StatusDeclaration } from "./status.enum";
 
 export class DeclarationService {
   private declarationRepository: DeclarationRepository;
@@ -18,18 +19,17 @@ export class DeclarationService {
    * @returns The declaration object.
    */
   async createDeclaration(declaration: DeclarationDtoIn) {
-    if (
-      declaration.declarationTeamMateId &&
+    if (!Object.values(StatusDeclaration).includes(declaration.declarationStatus)) {
+      throw new TypeError("Invalid declaration status.");
+    } if (declaration.declarationTeamMateId &&
       declaration.declarationDate &&
       declaration.declarationStatus !== undefined &&
-      declaration.declarationStatus.trim() !== ""
-    ) {
+      declaration.declarationStatus.trim() !== "") {
       return await this.declarationRepository.createDeclaration(declaration);
     } else {
       throw new TypeError("One or more mandatory information is missing.");
     }
   }
-
 
   /**
    * Get all declarations for a team mate.
@@ -38,13 +38,13 @@ export class DeclarationService {
    */
   async getDeclarationForTeamMate(teamMateId: number): Promise<DeclarationDtoIn | String> {
     const declaration = await this.declarationRepository.getDeclarationForTeamMate(teamMateId);
-    if (declaration !== null || declaration !== undefined)  {
+    if (declaration !== null || declaration !== undefined) {
       return declaration;
     } else {
       throw new TypeError("You have to declare yourself.");
     }
   }
-  
+
   /**
    * This function updates the status of a declaration.
    * @param {number} declarationId - number,
@@ -55,6 +55,13 @@ export class DeclarationService {
     const existingDeclaration = await this.declarationRepository.getDeclarationById(declarationId);
     if (!existingDeclaration) {
       throw new TypeError("The declaration does not existe.");
+    }
+    if (declaration.declarationTeamMateId === null ||
+      declaration.declarationDate === null ||
+      declaration.declarationStatus.trim() === "") {
+      throw new TypeError("One or more mandatory information is missing.");
+    } if (!Object.values(StatusDeclaration).includes(declaration.declarationStatus)) {
+      throw new TypeError("Invalid declaration status.");
     }
     return this.declarationRepository.updateDeclarationStatus(declarationId, declaration);
   }
