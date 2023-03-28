7e7e569baf3ad992e2b2b66cc7152c8e86ee8fe8
profotoce59 <romain.vanhee@gmail.com>
2023-03-28 14:34:11 +0200

    feat(captain_get): dockerisation of the spring boot

--- yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplicationTests.java
+++ yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class YakiAdminBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
