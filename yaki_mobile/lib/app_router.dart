bff68289bc7fc3edb40dfe7fcbae0e94b65f86dd
Rym <rymmihoub@gmail.com>
2023-04-06 17:29:26 +0200

    feat(header_day_declaration): modified header day declaration and add navigation

--- yaki_mobile/lib/app_router.dart
+++ yaki_mobile/lib/app_router.dart
@@ -29,6 +29,17 @@ final goRouterProvider = Provider<GoRouter>(
                 }
               },
             ),
+            //GoRoute(
+              //path: 'morningDeclaration',
+              //builder: (context) => const MorningDeclaration(),
+              //redirect: (BuildContext context) async {
+                //if (await SharedPref.isTokenPresent()) {
+                  //return '/morningDeclaration';
+                //} else {
+                  //return '/';
+                //}
+              //},
+            //),
             GoRoute(
               path: 'morningDeclaration',
               builder: (context, state) => const MorningDeclaration(),
