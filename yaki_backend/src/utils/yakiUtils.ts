b0b41fbb59f7b43e978c68a880c8528039789930
Laurent <magrez0laurent@gmail.com>
2023-04-07 09:08:59 +0200

    feat(POST): halfday postgres query working, utils method functions addition

---
+++ yaki_backend/src/utils/yakiUtils.ts
@@ -0,0 +1,52 @@
+export default class YakiUtils {
+  /**
+   * Transform a object list into a flat array containing only the objects values.
+   * @param objectList
+   * @returns Array<String>
+   */
+  static objectsListToValuesList(objectList: Array<object>): Array<string> {
+    const flatValuesList = objectList
+      .map((obj) => {
+        return Object.values(obj);
+      })
+      .flat();
+    return flatValuesList;
+  }
+
+  /**
+   * Create the string used for postGres INSERT query VALUES string.
+   * Use complete values list to insert to determine VALUES count.
+   * (*Need to convert any object or Array<object> into flat values list*)
+   *
+   * And reference obj to determine how many rows to insert.
+   *
+   *  *Check if reach array end to break loop, before step check.*
+   *  *Prevent to add "()" at string end in situations where arr.length % step === 0*
+   * @param arr reference list to produces VALUES
+   * @param obj reference object
+   * @returns string like : ($1, $2...), ($x, $y...),...
+   */
+  static createInsertValues(arr: Array<any>, obj: object): string {
+    const arrLength: number = arr.length;
+    const step: number = Object.values(obj).length;
+    let postgresValues: string = "(";
+
+    for (let i = 1; i <= arrLength; i++) {
+      postgresValues += "$" + i;
+
+      if (i % step !== 0 && i !== arrLength) {
+        postgresValues += ", ";
+      }
+
+      if (i === arrLength) {
+        postgresValues += ")";
+        break;
+      }
+
+      if (i % step === 0) {
+        postgresValues += "), (";
+      }
+    }
+    return postgresValues;
+  }
+}
