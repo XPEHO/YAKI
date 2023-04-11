eafab20c5fa319b7e532a2776c22840cb26c9adc
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-04-11 17:28:01 +0200

    feat(halfday-declaration): removed dead code

--- yaki_mobile/lib/presentation/ui/declaration/afternoon_declaration.dart
+++ yaki_mobile/lib/presentation/ui/declaration/afternoon_declaration.dart
@@ -25,12 +25,3 @@ class AfternoonDeclaration extends StatelessWidget {
     );
   }
 }
-
-class Test extends StatelessWidget {
-  const Test({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
--- yaki_mobile/lib/presentation/ui/declaration/morning_declaration.dart
+++ yaki_mobile/lib/presentation/ui/declaration/morning_declaration.dart
@@ -25,12 +25,3 @@ class MorningDeclaration extends StatelessWidget {
     );
   }
 }
-
-class Test extends StatelessWidget {
-  const Test({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
