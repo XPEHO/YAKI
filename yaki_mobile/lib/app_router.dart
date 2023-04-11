db6a466e0f5ec59801a64b3ac5a197777ee5725b
HaddoucheRym <107027392+HaddoucheRym@users.noreply.github.com>
2023-04-11 14:32:31 +0200

    Feat/header day declaration (#222)
    * feat(header_day_declaration): modified header day declaration and add navigation
    
    * feat(header): on progress
    
    * feat(header): the navigation between the declaration for full-day and half-day has been set up
    
    * feat(header): add scroll function to declarationBody
    
    * feat(header): analyz flutter and build
    
    * feat(header): modified translations files
    
    * feat(header): formate the code after rebase

--- yaki_mobile/lib/app_router.dart
+++ yaki_mobile/lib/app_router.dart
@@ -52,6 +52,28 @@ final goRouterProvider = Provider<GoRouter>(
               },
             ),
             GoRoute(
+              path: 'morningDeclaration',
+              builder: (context, state) => const MorningDeclaration(),
+              redirect: (BuildContext context, GoRouterState state) async {
+                if (await SharedPref.isTokenPresent()) {
+                  return '/morningDeclaration';
+                } else {
+                  return '/';
+                }
+              },
+            ),
+            GoRoute(
+              path: 'afternoonDeclaration',
+              builder: (context, state) => const AfternoonDeclaration(),
+              redirect: (BuildContext context, GoRouterState state) async {
+                if (await SharedPref.isTokenPresent()) {
+                  return '/afternoonDeclaration';
+                } else {
+                  return '/';
+                }
+              },
+            ),
+            GoRoute(
               path: 'status',
               builder: (context, state) => const Status(),
               redirect: (BuildContext context, GoRouterState state) async {
