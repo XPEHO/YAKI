6c0694a984c42cf9e7439a1cc628ca7df00dd1f1
profotoce59 <romain.vanhee@gmail.com>
2023-04-14 14:24:23 +0200

    feat(team_crud): modify indentation

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/services/TeamServiceImpl.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/services/TeamServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @Service
 public class TeamServiceImpl implements TeamService {
+
     final TeamJpaRepository teamJpaRepository;
 
     public TeamServiceImpl(TeamJpaRepository teamJpaRepository) {
