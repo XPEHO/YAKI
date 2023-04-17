a9da378309b06f9dba3b5163088b1706eba33990
profotoce59 <romain.vanhee@gmail.com>
2023-04-17 11:54:21 +0200

    feat(teammate_crud): first commit

---
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/models/TeammateModel.java
@@ -0,0 +1,2 @@
+package com.xpeho.yaki_admin_backend.data.models;public class TeammateModel {
+}
---
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/sources/TeammateJpaRepository.java
@@ -0,0 +1,2 @@
+package com.xpeho.yaki_admin_backend.data.sources;public interface TeammateJpaRepository {
+}
