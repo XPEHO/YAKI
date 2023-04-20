f8fadae72042fa4f803e461faa33eb6c450e41c1
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-04-20 10:37:39 +0200

    feat(captain_crud): fix evils

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/CaptainController.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/CaptainController.java
@@ -2,8 +2,6 @@ package com.xpeho.yaki_admin_backend.presentation.controllers;
 
 import com.xpeho.yaki_admin_backend.domain.entities.CaptainEntity;
 import com.xpeho.yaki_admin_backend.domain.services.CaptainService;
-import com.xpeho.yaki_admin_backend.domain.services.CustomerService;
-import com.xpeho.yaki_admin_backend.domain.services.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,14 +13,8 @@ import java.util.List;
 public class CaptainController {
     final CaptainService captainService;
 
-    final UserService userService;
-
-    final CustomerService customerService;
-
-    public CaptainController(CaptainService captainService, UserService userService, CustomerService customerService) {
+    public CaptainController(CaptainService captainService) {
         this.captainService = captainService;
-        this.userService = userService;
-        this.customerService = customerService;
     }
 
     @GetMapping
