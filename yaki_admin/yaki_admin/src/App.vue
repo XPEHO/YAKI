4255d77e2914b8dfc13d58558a91183235d841ca
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-04-20 12:05:01 +0200

    chore(initialize_front): create structure

---
+++ yaki_admin/yaki_admin/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <div>
+      <HelloWorld />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
---
+++ yaki_admin/yaki_admin/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="green">Hello world</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+</style>
