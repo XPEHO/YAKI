6572027fc198e8c6b7fea0200ee57e56c60bfb07
profotoce59 <romain.vanhee@gmail.com>
2023-04-26 11:39:36 +0200

    chore(test_karate): working test without docker compose

--- yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplicationTests.java
+++ yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplicationTests.java
@@ -3,11 +3,12 @@ package com.xpeho.yaki_admin_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class YakiAdminBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
