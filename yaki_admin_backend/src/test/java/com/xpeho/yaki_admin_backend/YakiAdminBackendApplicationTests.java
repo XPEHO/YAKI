c0a3bc67aef6357e69713635027ae85e822f6499
Romain Vanhée <71013671+profotoce59@users.noreply.github.com>
2023-04-28 10:15:41 +0200

    chore(karate_admin): remove blank lines

--- yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplicationTests.java
+++ yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplicationTests.java
@@ -3,12 +3,10 @@ package com.xpeho.yaki_admin_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class YakiAdminBackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
