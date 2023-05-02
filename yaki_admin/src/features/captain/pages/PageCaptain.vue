21e442b3e184cdf458b82eb2f258d156fee8d5da
Laurent <95299697+LaurentMag@users.noreply.github.com>
2023-05-02 15:03:43 +0200

    feat(admin-layout): set modular page layout (#323)
    Co-authored-by: laurent <laurentmag@MBPro-Xpeho-L.local>

---
+++ yaki_admin/src/features/captain/pages/PageCaptain.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import PageLayout from "@/shared/layouts/PageLayout.vue";
+import SideBarCaptainContent from "../components/SideBarCaptainContent.vue";
+import ContentCaptain from "@/features/captain/layouts/ContentCaptain.vue";
+</script>
+
+<template>
+  <PageLayout>
+    <template v-slot:slidebarContent>
+      <SideBarCaptainContent />
+    </template>
+
+    <template v-slot:content>
+      <ContentCaptain />
+    </template>
+  </PageLayout>
+</template>
+
+<style lang="scss"></style>
---
+++ yaki_admin/src/shared/components/SideBar.vue
@@ -0,0 +1,21 @@
+PageCaptain
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="router">
+    <slot></slot>
+  </nav>
+</template>
+
+<style lang="scss">
+// see asset/style.scss for #app { style }
+
+.router {
+  background-color: rgb(43, 41, 41);
+
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+</style>
