70e7c080cf03e730e4dc520dc5af1bae8c130b66
Laurent <95299697+LaurentMag@users.noreply.github.com>
2023-05-02 15:03:43 +0200

    feat(admin-layout): set modular page layout (#323)
    Co-authored-by: laurent <laurentmag@MBPro-Xpeho-L.local>

--- yaki_admin/src/main.ts
+++ yaki_admin/src/main.ts
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import {createApp} from "vue";
+import App from "./App.vue";
+import router from "./router/router";
 
-const app = createApp(App)
+import "./assets/style.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
---
+++ yaki_admin/src/shared/components/HeaderYaki.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header-test-two">
+    <p>Team</p>
+  </header>
+</template>
+
+<style lang="scss">
+.header-test-two {
+  background-color: #32353c;
+  display: flex;
+  align-items: center;
+
+  padding: 1rem;
+
+  p {
+    padding: 0.5rem;
+    color: rgb(222, 217, 217);
+    font-size: 1.5rem;
+    font-weight: 500;
+  }
+}
+</style>
