af84d0cf89015e7bd668af1c4aec620029660c01
Laurent <95299697+LaurentMag@users.noreply.github.com>
2023-05-02 15:03:43 +0200

    feat(admin-layout): set modular page layout (#323)
    Co-authored-by: laurent <laurentmag@MBPro-Xpeho-L.local>

---
+++ yaki_admin/src/features/captain/components/SideBarCaptainContent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+import AddTeamButton from "@/shared/components/AddTeamButton.vue";
+
+const teams = reactive({
+  list: [1, 2, 3, 4, 5],
+});
+
+const addTeam = () => {
+  teams.list.push(teams.list.length + 1);
+};
+</script>
+
+<template>
+  <p>My Team</p>
+
+  <div v-for="number in teams.list">{{ number }}</div>
+
+  <AddTeamButton />
+</template>
+
+<style lang="scss">
+p {
+  text-decoration: none;
+  color: rgb(186, 182, 182);
+  font-size: 1.2rem;
+  font-weight: 900;
+}
+</style>
---
+++ yaki_admin/src/shared/layouts/PageLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HeaderYaki from "../../shared/components/HeaderYaki.vue";
+import SideBar from "../components/SideBar.vue";
+import ContentCaptain from "@/features/captain/layouts/ContentCaptain.vue";
+</script>
+
+<template>
+  <header-yaki />
+  <section class="sidebar-content-container-grid">
+    <SideBar>
+      <slot name="slidebarContent"></slot>
+    </SideBar>
+    <slot name="content"></slot>
+  </section>
+</template>
+
+<style lang="scss">
+.sidebar-content-container-grid {
+  // flex 1 to have this container take the rest of the screen space
+  flex: 1;
+
+  display: grid;
+  grid-template-columns: minmax(10rem, 20rem) minmax(80%, 1fr);
+}
+</style>
