a4d41a59d1573ec9da3a35003f1c1d053219044a
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-05-03 10:37:03 +0200

    fix(declaration-page-responsive): removed unecessary classes

--- yaki_mobile/lib/presentation/ui/declaration/views/status_card.dart
+++ yaki_mobile/lib/presentation/ui/declaration/views/status_card.dart
@@ -17,144 +17,52 @@ class StatusCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraint) {
-        if (constraint.maxWidth < 350) {
-          return SmallStatusButton(
-            statusPicto: statusPicto,
-            statusName: statusName,
-            onPress: onPress,
-          );
-        } else {
-          return BigStatusButton(
-            statusPicto: statusPicto,
-            statusName: statusName,
-            onPress: onPress,
-          );
-        }
+        return SizedBox(
+          height: constraint.maxWidth < 350 ? 120 : 160,
+          width: constraint.maxWidth < 350 ? 120 : 160,
+          child: ElevatedButton(
+            onPressed: onPress,
+            style: ElevatedButton.styleFrom(
+              elevation: 5,
+              shadowColor: Colors.black,
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.amber.shade400,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            child: Stack(
+              children: [
+                Center(
+                  child: FractionallySizedBox(
+                    widthFactor: 0.55,
+                    heightFactor: 0.55,
+                    alignment: FractionalOffset.center,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: SvgPicture.asset(statusPicto),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      statusName,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
       },
-    );
-  }
-}
-
-class SmallStatusButton extends StatelessWidget {
-  final String statusPicto;
-  final String statusName;
-  final VoidCallback onPress;
-
-  const SmallStatusButton({
-    super.key,
-    required this.statusPicto,
-    required this.statusName,
-    required this.onPress,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 120,
-      width: 120,
-      child: ElevatedButton(
-        onPressed: onPress,
-        style: ElevatedButton.styleFrom(
-          elevation: 5,
-          shadowColor: Colors.black,
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.amber.shade400,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-        child: Stack(
-          children: [
-            Center(
-              child: FractionallySizedBox(
-                widthFactor: 0.55,
-                heightFactor: 0.55,
-                alignment: FractionalOffset.center,
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: SvgPicture.asset(statusPicto),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Text(
-                  statusName,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class BigStatusButton extends StatelessWidget {
-  final String statusPicto;
-  final String statusName;
-  final VoidCallback onPress;
-
-  const BigStatusButton({
-    super.key,
-    required this.statusPicto,
-    required this.statusName,
-    required this.onPress,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 160,
-      width: 160,
-      child: ElevatedButton(
-        onPressed: onPress,
-        style: ElevatedButton.styleFrom(
-          elevation: 5,
-          shadowColor: Colors.black,
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.amber.shade400,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-        child: Stack(
-          children: [
-            Center(
-              child: FractionallySizedBox(
-                widthFactor: 0.55,
-                heightFactor: 0.55,
-                alignment: FractionalOffset.center,
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: SvgPicture.asset(statusPicto),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Text(
-                  statusName,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
