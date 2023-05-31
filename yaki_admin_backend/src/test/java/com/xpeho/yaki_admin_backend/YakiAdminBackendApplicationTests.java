ac5f03b9a53c7d16a5ffc2532bc3c3e74bf2ad7f
Priscillia Ammeux <96787811+PriscilliaAmmeux@users.noreply.github.com>
2023-05-31 16:42:11 +0200

    fix(code_smell_sonarqube): Add at least one assertion to this test case (#414)

--- yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplicationTests.java
+++ yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplicationTests.java
@@ -1,13 +1,20 @@
 package com.xpeho.yaki_admin_backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class YakiAdminBackendApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
-        
+        assertNotNull(applicationContext, "The application context should not be null");
     }
 }
