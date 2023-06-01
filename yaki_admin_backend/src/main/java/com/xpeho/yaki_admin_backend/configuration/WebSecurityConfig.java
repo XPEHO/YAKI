750df39c7d65f93ef01abc5c754167906ccd5c9f
profotoce59 <romain.vanhee@gmail.com>
2023-06-01 10:25:36 +0200

    fix(security_admin_back): disable the security password in the testing env

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configuration/WebSecurityConfig.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configuration/WebSecurityConfig.java
@@ -1,23 +1,26 @@
-package com.xpeho.yaki_admin_backend.Configuration;
+package com.xpeho.yaki_admin_backend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors();
-        http.authorizeRequests()
-                .requestMatchers("/login/**")
-                .permitAll() // Allow access to login URLs to everyone
-                .anyRequest().permitAll(); //We can in the future require authentification for any other URL
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/**").permitAll() // Allow access to login URLs to everyone
+                .anyRequest().permitAll();
+        //We can in the future require authentification for any other URL
+        //we can also require ROLE for certain URL
         return http.build();
     }
 
@@ -27,4 +30,6 @@ public class WebSecurityConfig {
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
     }
+    //we allow everything for now but in a production env we should customize to authorize only
+    //some URL patterns
 }
