329a302b730d16e310893c19a4ca9c91c9f2b00b
Romain Vanhée <71013671+profotoce59@users.noreply.github.com>
2023-06-01 11:03:45 +0200

    fix(security_back_admin): add configurations for cors (#413)
    * fix(security_back_admin): add configurations for cors
    
    * fix(security_admin_back): disable the security password in the testing env
    
    * fix(security_admin_back): disable the security password in the testing env
    
    * fix(security_admin_back): update deprecated methods

---
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configuration/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.xpeho.yaki_admin_backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+        http.authorizeHttpRequests()
+                .requestMatchers("/**").permitAll() // Allow access to login URLs to everyone
+                .anyRequest().permitAll();
+        //We can in the future require authentification for any other URL
+        //we can also require ROLE for certain URL
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+    //we allow everything for now but in a production env we should customize to authorize only
+    //some URL patterns
+}
