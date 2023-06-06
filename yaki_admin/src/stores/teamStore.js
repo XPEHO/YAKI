301e6bb307605c5af5e8fa3c6d0fc4f4546da5cb
laurent <laurentmag@MBPro-Xpeho-L.local>
2023-06-06 17:26:57 +0200

    fix(front_build):Fix incorrect front end build, add .env, changed workflow

--- yaki_admin/src/stores/teamStore.js
+++ yaki_admin/src/stores/teamStore.js
@@ -1,19 +1,20 @@
-import {defineStore} from 'pinia'
-import { teamMateService } from '@/services/teamMate.service';
-export const useTeamStore = defineStore('teamStore',{
+import {defineStore} from "pinia";
+import {teamMateService} from "@/services/teamMate.service";
+
+export const useTeamStore = defineStore("teamStore", {
   state: () => ({
     team: 0,
-    teammate : []
+    teammate: [],
   }),
   getters: {
     getTeam() {
-      return this.team
+      return this.team;
     },
   },
   actions: {
     async setTeam(team) {
-      this.team = team
+      this.team = team;
       this.teammate = await teamMateService.getAllWithinTeam(this.team);
     },
   },
-})
+});
