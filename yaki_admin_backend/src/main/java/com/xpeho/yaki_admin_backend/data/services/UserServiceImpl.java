67684c4352a55de2cef22f84d8826380634eacf5
profotoce59 <romain.vanhee@gmail.com>
2023-06-07 18:52:05 +0200

    feat(user_crud): add id

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/services/UserServiceImpl.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/services/UserServiceImpl.java
@@ -19,7 +19,6 @@ public class UserServiceImpl implements UserService {
         this.userJpaRepository = userJpaRepository;
     }
 
-
     @Override
     public UserEntity save(UserEntityIn user) {
         final UserModel userModel = new UserModel(user.id(), user.lastname(), user.firstname(), user.email(), user.login(), user.password());
