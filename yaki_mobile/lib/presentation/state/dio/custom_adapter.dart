8f16e15e550339e8fd3d61a3b1bb3b47ea64e52e
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-06-14 15:40:13 +0200

    fix(deploy-web): fixed syntax error

--- yaki_mobile/lib/presentation/state/dio/custom_adapter.dart
+++ yaki_mobile/lib/presentation/state/dio/custom_adapter.dart
@@ -1,15 +1,12 @@
 import 'package:dio/dio.dart';
-import 'package:yaki/presentation/state/dio/customer_adapter_stub.dart'
-    if (dart.library.io) 'package:yaki/presentation/state/dio/mobile_oriented_adapter.dart'
-    if (dart.libray.js) 'package:yaki/presentation/state/dio/web_oriented_adapter.dart';
+import 'package:yaki/presentation/state/dio/mobile_oriented_adapter.dart'
+    if (dart.library.js) 'package:yaki/presentation/state/dio/web_oriented_adapter.dart';
 
 abstract class CustomAdapter {
   static CustomAdapter? _instance;
 
   static CustomAdapter? get instance {
-    print('custom_adapter');
     _instance ??= getAdapter();
-    print('customer_adapter: $_instance');
     return _instance;
   }
 
--- yaki_mobile/lib/presentation/state/dio/customer_adapter_stub.dart
+++
@@ -1,6 +0,0 @@
-import 'package:yaki/presentation/state/dio/custom_adapter.dart';
-
-CustomAdapter getAdapter() {
-  print('customer_adapter_stub.dart');
-  throw UnsupportedError('Cannot create an custom httpClientAdapter');
-}
