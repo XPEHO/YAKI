855712aedbd2d93aa26a817bf506b6cc029c4da9
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-06-16 13:56:42 +0200

    feat(athentication): configuration security jwt authent

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplication.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/YakiAdminBackendApplication.java
@@ -7,5 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class YakiAdminBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(YakiAdminBackendApplication.class, args);
+
+
     }
+
+
 }
