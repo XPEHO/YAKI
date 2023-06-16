47d2f3c0b031a3ad49c8a848aefffeb39b284237
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-06-16 13:56:42 +0200

    feat(athentication): configuration security jwt authent

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/sources/UserJpaRepository.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/sources/UserJpaRepository.java
@@ -3,6 +3,10 @@ package com.xpeho.yaki_admin_backend.data.sources;
 import com.xpeho.yaki_admin_backend.data.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserJpaRepository extends JpaRepository<UserModel, Integer> {
+    Optional<UserModel> findByEmail(String email);
+
 
 }
