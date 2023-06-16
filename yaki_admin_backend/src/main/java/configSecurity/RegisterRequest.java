763b90849773187d91b5c63f1b66c26bcb9cd35e
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-06-16 13:56:42 +0200

    feat(athentication): configuration security jwt authent

---
+++ yaki_admin_backend/src/main/java/configSecurity/RegisterRequest.java
@@ -0,0 +1,18 @@
+package configSecurity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
