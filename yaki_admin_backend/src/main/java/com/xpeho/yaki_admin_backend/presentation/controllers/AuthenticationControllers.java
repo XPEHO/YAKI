15cf929b81b2dd26b948a00967e7f346ba9df966
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-06-16 13:56:42 +0200

    feat(athentication): configuration security jwt authent

---
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/AuthenticationControllers.java
@@ -0,0 +1,33 @@
+package com.xpeho.yaki_admin_backend.presentation.controllers;
+
+import configSecurity.AuthenticationRequest;
+import configSecurity.AuthenticationResponse;
+import configSecurity.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+@RequiredArgsConstructor
+public class AuthenticationControllers {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
