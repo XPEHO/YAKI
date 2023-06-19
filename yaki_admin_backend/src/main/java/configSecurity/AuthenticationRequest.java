6f04d2f1ae626c4c68f06b0c9ba8615407165da1
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-06-19 15:25:47 +0200

    feat(athentication): authentication register

--- yaki_admin_backend/src/main/java/configSecurity/AuthenticationRequest.java
+++ yaki_admin_backend/src/main/java/configSecurity/AuthenticationRequest.java
@@ -1,16 +1,29 @@
 package configSecurity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class AuthenticationRequest {
-
-    String password;
+    private String password;
     private String email;
+    public AuthenticationRequest(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
