38beccfad22cdf92b69db8a23749db4267a6e057
profotoce59 <romain.vanhee@gmail.com>
2023-06-20 17:18:54 +0200

    feat(athentication): resolve problems

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/ApplicationConfig.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/ApplicationConfig.java
@@ -24,7 +24,7 @@ public class ApplicationConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> (UserDetails) jpaRepository.findByEmail(username)
+        return username -> (UserDetails) jpaRepository.findByLogin(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
