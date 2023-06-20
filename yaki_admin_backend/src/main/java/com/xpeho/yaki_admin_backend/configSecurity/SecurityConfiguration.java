afb70ab814f17204c79284475f39431c1bec32e0
profotoce59 <romain.vanhee@gmail.com>
2023-06-20 11:40:07 +0200

    feat(athentication): resolve bug and add swagger accessibility

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/SecurityConfiguration.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/SecurityConfiguration.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -64,4 +62,5 @@ public class SecurityConfiguration {
                 );
         return http.build();
     }
+
 }
