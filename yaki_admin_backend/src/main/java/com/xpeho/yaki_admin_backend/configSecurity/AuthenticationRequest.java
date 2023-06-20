f11107a2510a3d43ffcae6a450311d0b869bbefb
profotoce59 <romain.vanhee@gmail.com>
2023-06-20 11:40:07 +0200

    feat(athentication): resolve bug and add swagger accessibility

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/AuthenticationRequest.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/AuthenticationRequest.java
@@ -1,5 +1,8 @@
 package com.xpeho.yaki_admin_backend.configSecurity;
 
+import lombok.Builder;
+
+@Builder
 public class AuthenticationRequest {
     private String password;
     private String login;
