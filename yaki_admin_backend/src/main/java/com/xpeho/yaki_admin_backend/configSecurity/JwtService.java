4ba0e3240d56ac1ed1aca96ab0b7b6ed173e0057
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-06-22 17:24:42 +0200

    feat(athentication): fix bugs @Bean

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/JwtService.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/JwtService.java
@@ -17,6 +17,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
+
     @Value("${token.signing.key}")
     private String secretKey;
 
@@ -51,7 +52,7 @@ public class JwtService {
         final String userName = extractUserName(token);
         return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
- 
+
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
