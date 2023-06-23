de4090306b77bc6ef2bdb3e37ed57f427913ddef
profotoce59 <romain.vanhee@gmail.com>
2023-06-23 17:26:09 +0200

    feat(athentication): add swagger config for csrf security

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/SecurityConfiguration.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/SecurityConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
