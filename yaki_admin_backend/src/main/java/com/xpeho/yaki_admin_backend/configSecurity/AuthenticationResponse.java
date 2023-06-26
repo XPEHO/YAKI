8dd37307a2dea34491ed5eed0c64c9908c882c6d
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-06-26 10:27:15 +0200

    feat(athentication): delete @autowired

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/AuthenticationResponse.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/AuthenticationResponse.java
@@ -1,9 +1,6 @@
 package com.xpeho.yaki_admin_backend.configSecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class AuthenticationResponse {
-    @Autowired
     private String token;
 
     public AuthenticationResponse(String token) {
