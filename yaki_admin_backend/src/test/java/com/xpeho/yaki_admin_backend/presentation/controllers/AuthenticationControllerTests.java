f84fde62446de72327d72ce73bc272cd44f7cd21
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-06-27 15:48:04 +0200

    feat(athentication): update gradle

--- yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/presentation/controllers/AuthenticationControllerTests.java
+++ yaki_admin_backend/src/test/java/com/xpeho/yaki_admin_backend/presentation/controllers/AuthenticationControllerTests.java
@@ -2,16 +2,12 @@ package com.xpeho.yaki_admin_backend.presentation.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xpeho.yaki_admin_backend.domain.services.AuthenticationService;
-import com.xpeho.yaki_admin_backend.error_handling.CustomExceptionHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class AuthenticationControllerTests {
@@ -26,12 +22,9 @@ class AuthenticationControllerTests {
     //creating mock of the controller before each test
     @BeforeEach
     public void setUp() {
-        mvc = MockMvcBuilders.standaloneSetup(authenticationController)
-                .setControllerAdvice(new CustomExceptionHandler())
-                .build();
-    }
 
     }
+
     //testing the authenticationController.post() register
     @Test
     void testRegister() throws Exception {
