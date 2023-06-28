424a37a23ae53b6e4d379e44ba4975bd0202e425
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-06-28 17:16:39 +0200

    feat(profile-page): added a log out button

--- yaki_mobile/lib/presentation/ui/profile/profile.dart
+++ yaki_mobile/lib/presentation/ui/profile/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:yaki/domain/entities/logged_user.dart';
 import 'package:yaki/presentation/state/providers/login_provider.dart';
 import 'package:yaki/presentation/ui/shared/pages_layout.dart';
@@ -21,7 +22,31 @@ class Profile extends ConsumerWidget {
           headerHint:
               user != null ? '${user.firstName} ${user.lastName}' : 'teamMate',
         ),
-        bodyContent: const Text('Coming soon'),
+        bodyContent: Center(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              elevation: 5,
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w600,
+              ),
+              padding: const EdgeInsets.only(
+                top: 20,
+                bottom: 20,
+                right: 50,
+                left: 50,
+              ),
+            ),
+            onPressed: () {
+              context.go('/');
+            },
+            child: const Text(
+              'Se déconnecter',
+            ),
+          ),
+        ),
       ),
     );
   }
