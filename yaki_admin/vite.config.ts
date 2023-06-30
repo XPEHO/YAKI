6c9c7d1afbe03fc3b2fcf73765a4edc1ea333b87
Laurent <95299697+LaurentMag@users.noreply.github.com>
2023-06-30 10:15:51 +0200

    feat(mobile_captain_query): sql query change for mobile captain view (#509)
    * feat(mobile_captain_query): sql query change
    
    * feat(mobile_captain_query): captain sql query fix for halfday
    
    * feat(mobile_captain_query): vue3 vite config fix
    
    ---------
    
    Co-authored-by: laurent <laurentmag@MBP-de-Laurent.lan>

--- yaki_admin/vite.config.ts
+++ yaki_admin/vite.config.ts
@@ -1,21 +1,19 @@
 import {fileURLToPath, URL} from "node:url";
 
-import {defineConfig, loadEnv} from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-  return {
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    test: {
-      coverage: {
-        provider: "istanbul", // or 'v8'
-      },
+  },
+  test: {
+    coverage: {
+      provider: "istanbul", // or 'v8'
     },
-  };
+  },
 });
