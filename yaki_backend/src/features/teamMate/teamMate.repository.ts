e6f86fb0bd5518cb97e32aa07e0dd37b4ab59678
laurent <laurentmag@MBP-de-Laurent.lan>
2023-06-30 08:40:25 +0200

    feat(mobile_captain_query): sql query change

--- yaki_backend/src/features/teamMate/teamMate.repository.ts
+++ yaki_backend/src/features/teamMate/teamMate.repository.ts
@@ -30,24 +30,34 @@ export class TeamMateRepository {
       port: Number(process.env.DB_PORT),
     });
     const query = `
-    SELECT 
-    user_id, team_mate_id, user_last_name, user_first_name, max_decl.declaration_date, max_decl.declaration_status
-    FROM public.user
-    INNER JOIN public.team_mate
-    ON user_id = team_mate_user_id
-    LEFT JOIN
-    (
-      SELECT * 
-      FROM (
-        SELECT declaration_team_mate_id, declaration_date, declaration_status, rank()
-        OVER (PARTITION BY declaration_team_mate_id ORDER BY declaration_date DESC)
-        FROM public.declaration
-      ) t
-      WHERE rank = 1
-    ) as max_decl
-    ON declaration_team_mate_id = team_mate.team_mate_id
-    WHERE team_mate_team_id = $1
-    AND declaration_date::date = now()::date;
+        SELECT user_id, team_mate_id, user_last_name, user_first_name,
+        CASE
+            WHEN declaration_date::date = now()::date 
+            OR (
+                declaration_date_start::date <= now()::date
+                AND declaration_date_end::date > now()::date 
+              )
+            THEN declaration_date
+            ELSE NULL
+        END AS declaration_date,
+        CASE
+            WHEN declaration_date::date = now()::date 
+            OR (
+                declaration_date_start::date <= now()::date
+                AND declaration_date_end::date > now()::date 
+              )
+            THEN declaration_status
+            ELSE NULL
+        END AS declaration_status
+        FROM (
+            SELECT u.user_id, t.team_mate_id, u.user_last_name, u.user_first_name, d.declaration_date AS declaration_date, d.declaration_date_start, d.declaration_date_end, d.declaration_status,
+            ROW_NUMBER() OVER (PARTITION BY u.user_id ORDER BY d.declaration_date DESC) AS rowCount
+            FROM public.user AS u
+            INNER JOIN public.team_mate AS t ON u.user_id = t.team_mate_user_id
+            LEFT JOIN public.declaration AS d ON t.team_mate_id = d.declaration_team_mate_id
+            WHERE t.team_mate_team_id = $1
+        ) AS subquery
+        WHERE rowCount = 1;
   `;
     client.connect();
     const poolResult: QueryResult = await client.query(query, [team_id]);
