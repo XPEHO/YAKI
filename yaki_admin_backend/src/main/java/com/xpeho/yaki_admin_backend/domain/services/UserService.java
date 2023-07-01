a38f7e1524d334310edcea783d927c4becf98191
laurent <laurentmag@MBP-de-Laurent.lan>
2023-07-01 12:59:10 +0200

    feat(vue3_user_to_team): add fetch user in ID range in admin_back

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/domain/services/UserService.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/domain/services/UserService.java
@@ -1,12 +1,18 @@
 package com.xpeho.yaki_admin_backend.domain.services;
 
+import com.xpeho.yaki_admin_backend.data.models.UserModel;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntity;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntityIn;
+import com.xpeho.yaki_admin_backend.domain.entities.UserEntityWithID;
+
+import java.util.List;
 
 public interface UserService {
 
     UserEntity save(UserEntityIn userEntityIn);
 
     UserEntity findById(int id);
+
+    List<UserEntityWithID> FindUserByIdRange(int idStart, int idEnd);
 
 }
--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.xpeho.yaki_admin_backend.presentation.controllers;
 
+import com.xpeho.yaki_admin_backend.data.models.UserModel;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntity;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntityIn;
+import com.xpeho.yaki_admin_backend.domain.entities.UserEntityWithID;
 import com.xpeho.yaki_admin_backend.domain.services.UserService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -19,5 +23,11 @@ public class UserController {
     @PostMapping
     public UserEntity createUser(@RequestBody UserEntityIn userEntity) {
         return userService.save(userEntity);
+    }
+
+    @GetMapping("/inRange")
+    public List<UserEntityWithID> FindUserByIdRange(@RequestParam int idStart,
+                                                    @RequestParam int idEnd) {
+        return userService.FindUserByIdRange(idStart, idEnd);
     }
 }
