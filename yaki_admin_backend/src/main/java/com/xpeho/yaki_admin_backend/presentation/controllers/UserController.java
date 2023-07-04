ad51029f93beb61ec42ab987b9d7034802b21ddd
PiotrFLEURY <piotr.fleury@gmail.com>
2023-07-04 11:42:08 +0200

    feat(authentication): implement spring security

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
@@ -1,10 +1,12 @@
 package com.xpeho.yaki_admin_backend.presentation.controllers;
 
+import com.xpeho.yaki_admin_backend.domain.entities.TeammateEntity;
 import com.xpeho.yaki_admin_backend.data.models.UserModel;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntity;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntityIn;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntityWithID;
 import com.xpeho.yaki_admin_backend.domain.services.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,11 +14,11 @@ import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping("/users")
+@SecurityRequirement(name = "bearerAuth")
 public class UserController {
-    final UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
-
         this.userService = userService;
     }
 
@@ -24,7 +26,10 @@ public class UserController {
     public UserEntity createUser(@RequestBody UserEntityIn userEntity) {
         return userService.save(userEntity);
     }
-
+    @DeleteMapping("{id}")
+    public UserEntity deleteUser(@PathVariable int id) {
+        return userService.deleteById(id);
+    }
     @GetMapping("/inRange")
     public List<UserEntityWithID> findUserByIdRange(@RequestParam int idStart,
                                                     @RequestParam int idEnd) {
