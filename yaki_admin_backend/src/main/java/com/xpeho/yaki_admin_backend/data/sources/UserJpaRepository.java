5db65783682cb3dd2538d0c828604dbd970bfbed
Piotr FLEURY <piotr.fleury@gmail.com>
2023-07-04 15:00:01 +0200

    test(karate): change karate tests for authentication (#530)
    * test(karate): change karate tests for authentication
    
    * test(karate_authent): fix double karate test
    
    * test(karate_authent): change to console.debug when docker compose
    
    ---------
    
    Co-authored-by: profotoce59 <romain.vanhee@gmail.com>

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/sources/UserJpaRepository.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/sources/UserJpaRepository.java
@@ -3,7 +3,7 @@ package com.xpeho.yaki_admin_backend.data.sources;
 import com.xpeho.yaki_admin_backend.data.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+
 import java.util.List;
 import java.util.Optional;
 
--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
@@ -1,12 +1,10 @@
 package com.xpeho.yaki_admin_backend.presentation.controllers;
 
-import com.xpeho.yaki_admin_backend.domain.entities.TeammateEntity;
 import com.xpeho.yaki_admin_backend.data.models.UserModel;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntity;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntityIn;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntityWithID;
 import com.xpeho.yaki_admin_backend.domain.services.UserService;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,11 +12,11 @@ import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping("/users")
-@SecurityRequirement(name = "bearerAuth")
 public class UserController {
-    private final UserService userService;
+    final UserService userService;
 
     public UserController(UserService userService) {
+
         this.userService = userService;
     }
 
@@ -26,10 +24,7 @@ public class UserController {
     public UserEntity createUser(@RequestBody UserEntityIn userEntity) {
         return userService.save(userEntity);
     }
-    @DeleteMapping("{id}")
-    public UserEntity deleteUser(@PathVariable int id) {
-        return userService.deleteById(id);
-    }
+
     @GetMapping("/inRange")
     public List<UserEntityWithID> findUserByIdRange(@RequestParam int idStart,
                                                     @RequestParam int idEnd) {
