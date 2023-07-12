ce483b164fa804d366d1bce5efeb7b6f99bfc703
profotoce59 <romain.vanhee@gmail.com>
2023-07-12 16:36:32 +0200

    fix(admin_front_search_teammates): add token for search user bar

--- yaki_admin/src/services/users.service.ts
+++ yaki_admin/src/services/users.service.ts
@@ -7,8 +7,13 @@ const URL: string = environmentVar.baseURL;
 
 export class UserService {
   fetchUserInRange = async (idStart: number, idEnd: number): Promise<UserWithIdType[]> => {
-    const response = await fetch(`${URL}/users/inRange?idStart=${idStart}&idEnd=${idEnd}`)
-      .then((res) => res.json())
+    const requestOptions = {
+      method: 'POST',
+      headers: authHeader(`${URL}/users/inRange?idStart=${idStart}&idEnd=${idEnd}`),
+      
+    }
+    const response = await fetch(`${URL}/users/inRange?idStart=${idStart}&idEnd=${idEnd}`,requestOptions)
+      .then(handleResponse)
       .catch((err) => console.warn(err));
 
     return response;
