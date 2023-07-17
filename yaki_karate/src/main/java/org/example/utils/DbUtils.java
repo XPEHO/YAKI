e94b30ef2aaa01f3f30b81636d95dc8c61eb4443
Romain Vanhée <71013671+profotoce59@users.noreply.github.com>
2023-07-17 15:08:35 +0200

    Test/karate connect database (#565)
    * test(karate_connection_database): add connection to database
    
    * test(karate_connect_database): assure that the connection to database is effectued
    
    * test(karate_connect_database): change workflow to fetch env file in karate
    
    * test(karate_connect_database): change workflow
    
    * test(karate_connect_database): change karate config to a .env
    
    * test(karate_connect_database): remove unused code

---
+++ yaki_karate/src/main/java/org/example/utils/DbUtils.java
@@ -0,0 +1,29 @@
+package org.example.utils;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DbUtils {
+
+    private final JdbcTemplate jdbc;
+    public DbUtils() {
+        Dotenv dotenv = Dotenv.load();
+        String driver = "org.postgresql.Driver";
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(dotenv.get("DB_URL"));
+        dataSource.setUsername(dotenv.get("DB_USER"));
+        dataSource.setPassword(dotenv.get("DB_PASSWORD"));
+        jdbc = new JdbcTemplate(dataSource);
+    }
+
+    public Object readValue(String query) {
+        return jdbc.queryForObject(query, Object.class);
+    }
+
+}
--- yaki_karate/src/test/resources/adminAuthenticate.feature
+++ yaki_karate/src/test/resources/adminAuthenticate.feature
@@ -3,9 +3,15 @@ Feature: Register
   Background:
     * url 'http://localhost:8080'
     * def schema = {id : '#number', userId : '#number', customerId : '#number' }
+    * def DbUtils = Java.type('org.example.utils.DbUtils')
+    * def db = new DbUtils()
 
   Scenario: Register
     Given path '/login/register'
     And request { lastname: 'owner', firstname: 'owner', email: 'owner@gmail.com', login: 'owner', password: 'owner' }
     When method post
     Then status 200
+
+  Scenario: Confirm That the connection to database is correct
+    * def token = db.readValue('SELECT user_id FROM public.user WHERE user_id = 1')
+    Then match token == 1
