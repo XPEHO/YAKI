5ab53959beabfbbf73d621f7f217c2f39cd17e96
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-07-17 16:57:15 +0200

    feature(pageCustomer): layout customer

--- yaki_admin/src/stores/captainStore.ts
+++ yaki_admin/src/stores/captainStore.ts
@@ -3,12 +3,57 @@ import { CaptainType } from "@/models/captain.type";
 import { captainService } from "@/services/captain.service";
 
 export const useCaptainStore = defineStore("captainStore", {
-    state: () => ({
-      customerId : [] as number[],
-      captainId: 0 as number,
-
-    }),
+  state: () => ({
+    customerId: [] as number[],
+    captainId: 0 as number,
+    captainList: [] as CaptainType[],
+    captainToDelete: 0 as number,
   }),
-  getters: {};
-actions: {
-}
+  getters: {
+    getCustomerId(): number[] {
+      return this.customerId;
+    },
+    getCaptainId(): number {
+      return this.captainId;
+    },
+    getCaptainList(): CaptainType[] {
+      return this.captainList;
+    },
+    getCaptainToDelete(): number {
+      return this.captainToDelete;
+    },
+  },
+
+  actions: {
+    setCaptainId(captainId: number) {
+      this.captainId = captainId;
+    },
+    setCustomerId(customerId: number[]) {
+      this.customerId = customerId;
+    },
+
+    // get all captains of a customer
+    async getAllCaptainsByCustomerId(customerId: number[]) {
+      this.captainList = [];
+      for (const id of customerId) {
+        const result = await captainService.getAllCaptainsByCustomerId(id);
+        this.captainList = this.captainList.concat(result);
+      }
+    },
+
+    // create a captain
+    async createCaptain(data: CaptainType): Promise<void> {
+      await captainService.createCaptain(data);
+    },
+
+    //get the captainId to delete
+    setCaptainToDelete(captainId: number) {
+      this.captainToDelete = captainId;
+    },
+
+    //delete a captain
+    async deleteCaptain(captainId: number): Promise<void> {
+      await captainService.deleteCaptain(captainId);
+    },
+  },
+});
