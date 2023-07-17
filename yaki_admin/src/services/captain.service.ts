ee899bc806a8bbaa1506a866d8f841fafd35a092
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-07-17 15:21:32 +0200

    feature(pageCustomer): create folders and files customerPage

--- yaki_admin/src/services/captain.service.ts
+++ yaki_admin/src/services/captain.service.ts
@@ -1,23 +1,55 @@
-import {environmentVar} from "@/envPlaceholder";
-import {CaptainType} from "@/models/captain.type";
+import { environmentVar } from "@/envPlaceholder";
+import { CaptainType } from "@/models/captain.type";
 import { authHeader } from "@/utils/authUtils";
 import { handleResponse } from "@/utils/responseUtils";
-
 
 const URL: string = environmentVar.baseURL;
 
 export class CaptainService {
-    getAllCaptainByUserId = async (userId: number): Promise<CaptainType[]> => {
-      const requestOptions = {
-        method: 'GET',
-        headers: authHeader(`${URL}/captains/user/${userId}`),
-        
-      }
-    const response = await fetch(`${URL}/captains/user/${userId}`,requestOptions)
+  getAllCaptainByUserId = async (userId: number): Promise<CaptainType[]> => {
+    const requestOptions = {
+      method: "GET",
+      headers: authHeader(`${URL}/captains/user/${userId}`),
+    };
+    const response = await fetch(
+      `${URL}/captains/user/${userId}`,
+      requestOptions
+    )
       .then(handleResponse)
       .catch((err) => console.warn(err));
 
     return response;
+  };
+
+  getAllCaptainsByCustomerId = async (
+    customerId: number
+  ): Promise<CaptainType[]> => {
+    const requestOptions = {
+      method: "GET",
+      headers: authHeader(`${URL}/captains/customer/${customerId}`),
+    };
+    const response = await fetch(
+      `${URL}/captains/customer/${customerId}`,
+      requestOptions
+    )
+      .then(handleResponse)
+      .catch((err) => console.warn(err));
+
+    return response;
+  };
+
+  // create a captain
+  createCaptain = async (data: CaptainType): Promise<CaptainType> => {
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: authHeader(`${URL}/captains/create`),
+    };
+    await fetch(`${URL}/captains/create`, requestOptions)
+      .then(handleResponse)
+      .catch((err) => console.warn(err));
+
+    return data;
   };
 }
 
