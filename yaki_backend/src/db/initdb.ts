657bb7da627a59eb918645d56e31e1b109b2a94d
laurent <laurentmag@MBP-de-Laurent.lan>
2023-07-26 15:10:29 +0200

    fix(back_mobile): remove create table & starting data from mobile api

--- yaki_backend/src/db/initdb.ts
+++ yaki_backend/src/db/initdb.ts
@@ -1,24 +1,18 @@
-import fs from 'fs'
-import { Pool } from 'pg';
-import { createUser } from './create_user';
+import {Pool} from "pg";
+import {createUser} from "./create_user";
 
-const readSqlFile = (file : string) => {
-    return fs.readFileSync(file, 'utf8');
-}
+export const initdb = async () => {
+  let createUserInDB = createUser;
 
-export const initdb = async() => {
-    let createTable = createUser + readSqlFile('./src/db/create_tables.sql');
-    let insertContent = createTable + readSqlFile('./src/db/starting_data.sql');
+  const pool = new Pool({
+    user: `${process.env.DB_USER}`,
+    host: `${process.env.DB_HOST}`,
+    database: `${process.env.DB_DATABASE}`,
+    password: `${process.env.DB_PASSWORD}`,
+    port: Number(process.env.DB_PORT),
+  });
 
-    const pool = new Pool({
-        user: `${process.env.DB_USER}`,
-        host: `${process.env.DB_HOST}`,
-        database: `${process.env.DB_DATABASE}`,
-        password: `${process.env.DB_PASSWORD}`,
-        port:  Number(process.env.DB_PORT)
-    });
+  await pool.query(createUserInDB);
 
-    await pool.query(insertContent);
-
-    await pool.end();
-}
+  await pool.end();
+};
