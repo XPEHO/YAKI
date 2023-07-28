543e3c54c2f6d04dd2d6d222504c6f5206e9f2a8
Romain Vanhée <71013671+profotoce59@users.noreply.github.com>
2023-07-28 13:10:17 +0200

    feat(fix_cors): add a route to get all customer with right (#651)

--- yaki_admin/src/stores/authStore.ts
+++ yaki_admin/src/stores/authStore.ts
@@ -9,13 +9,13 @@ import router from "@/router/router";
 import { useTeamStore } from "@/stores/teamStore.js";
 import { useCustomerRightsStore } from "@/stores/customerRightsStore.js";
 
-import { CustomersRightsType } from "@/models/customersRights.type";
 import { CaptainType } from "@/models/captain.type";
+import { CustomerType } from "@/models/customer.type";
 
 export const useAuthStore = defineStore("loginStore", {
   state: () => ({
     captains: [] as CaptainType[],
-    customersRights: [] as CustomersRightsType[],
+    customersRights: [] as CustomerType[],
     user: (() => {
       const userString = localStorage.getItem("user");
       return userString ? JSON.parse(userString) : "";
@@ -30,7 +30,7 @@ export const useAuthStore = defineStore("loginStore", {
         this.captains = await captainService.getAllCaptainByUserId(
           this.user.id
         );
-        this.customersRights = await customerService.getAllCustomersRightById(
+        this.customersRights = await customerService.getAllCustomersRightByUserId(
           this.user.id
         );
         //if the user is not a captain or a customer, he can't access to the admin part
@@ -38,6 +38,12 @@ export const useAuthStore = defineStore("loginStore", {
           console.log("not captain or customer");
           this.logout();
           return false;
+        }
+        if(this.customersRights.length >= 1){
+          this.returnedUrl = "/administration/customer"
+        }
+        else{ //if not a customer it's necessarily a captain
+          this.returnedUrl ="/administration/captain"
         }
         let idsCust = [];
         let idsCap = [];
@@ -51,11 +57,7 @@ export const useAuthStore = defineStore("loginStore", {
         const customerRightsStore = useCustomerRightsStore();
         teamStore.setCaptainsId(idsCap);
         customerRightsStore.setCustomersRightsId(idsCust);
-        router.push(
-          this.returnedUrl ||
-            "/administration/captain" ||
-            "/administration/customer"
-        );
+        router.push(this.returnedUrl);
         return true;
       } catch {
         return false;
