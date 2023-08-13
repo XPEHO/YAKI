68a46a343e423cce8f5c1f373f187d1992382e31
laurent <laurentmag@MBP-de-Laurent.lan>
2023-08-13 21:11:06 +0200

    fix(mobile_popup): fixing declaration

--- yaki_mobile/test/unit/data/models/declaration_model_test.dart
+++ yaki_mobile/test/unit/data/models/declaration_model_test.dart
@@ -5,19 +5,19 @@ import 'package:yaki/data/models/declaration_model.dart';
 void main() {
   // simulate json send by API
   Map<String, dynamic> declarationModelAsJson = {
+    "declarationUserId": 1,
     "declarationDate": '2023-03-20T10:00:00.950Z',
     "declarationDateStart": '2023-03-20T00:00:00.000Z',
     "declarationDateEnd": '2023-03-20T23:59:59.950Z',
-    "declarationTeamMateId": 3,
     "declarationStatus": 'VACATION',
     "declarationTeamId": 2,
   };
   // model instance once json is parsed into dart usable model
   DeclarationModel declarationModel = DeclarationModel(
+    declarationUserId: 1,
     declarationDate: DateTime.parse('2023-03-20T10:00:00.950Z'),
     declarationDateStart: DateTime.parse('2023-03-20T00:00:00.000Z'),
     declarationDateEnd: DateTime.parse('2023-03-20T23:59:59.950Z'),
-    declarationTeamMateId: 3,
     declarationStatus: 'VACATION',
     declarationTeamId: 2,
   );
