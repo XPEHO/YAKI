cf2fefe9cd92fa8b5be2300fa1ebe93be5080d7b
profotoce59 <romain.vanhee@gmail.com>
2023-08-18 14:49:18 +0200

    fix(mobile_validation_register): reverse unwanted changes

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/models/UserModel.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/models/UserModel.java
@@ -134,17 +134,17 @@ public class UserModel implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
 
