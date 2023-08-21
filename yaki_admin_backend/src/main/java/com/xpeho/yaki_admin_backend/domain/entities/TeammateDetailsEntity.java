202e51113f4e6c8163ed366fb39218f3d56ca369
laurent <laurentmag@MacBook-Pro-de-Laurent.local>
2023-08-21 10:53:27 +0200

    fix(vue_archi): refactorisation

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/domain/entities/TeammateDetailsEntity.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/domain/entities/TeammateDetailsEntity.java
@@ -1,3 +1,3 @@
 package com.xpeho.yaki_admin_backend.domain.entities;
 
-public record TeammateDetailsEntity(int id, int teamId, int userId, String firstName, String lastName, String email) {}
+public record TeammateDetailsEntity(int id, int teamId, int userId, String firstname, String lastname, String email) {}
