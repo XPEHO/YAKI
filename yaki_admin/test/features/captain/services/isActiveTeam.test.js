f87806c93c8f8ae136c6b7ffdcbf21bbcf715184
laurent <laurentmag@MacBook-Pro-de-Laurent.local>
2023-08-24 17:11:04 +0200

    fix(vue3_archi): fixed store connexion

---
+++ yaki_admin/test/features/captain/services/isActiveTeam.test.js
@@ -0,0 +1,9 @@
+import {test, it, expect} from "vitest";
+import {isSelectedTeamActive} from "../../../../src/features/shared/services/isSelectedTeamActive.ts";
+
+test("isSelectedTeamActive", () => {
+  test("isSameIndex", () => {
+    // Assert
+    expect(true).toBe(true);
+  });
+});
