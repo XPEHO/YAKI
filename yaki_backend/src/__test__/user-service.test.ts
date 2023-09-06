9142d0a5ac1468b738f70e530b6045b47c9b6407
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-09-06 11:14:19 +0200

    feat(resetPassword): test

--- yaki_backend/src/__test__/user-service.test.ts
+++ yaki_backend/src/__test__/user-service.test.ts
@@ -1,9 +1,10 @@
-import {UserService} from "../features/user/user.service";
-import {UserRepository} from "../features/user/user.repository";
+import { UserService } from "../features/user/user.service";
+import { UserRepository } from "../features/user/user.repository";
 import mockDb from "./__mocks__/mockDbUsers";
 import UserModel from "../features/user/user.dtoIn";
-import {CaptainDtoOut} from "../features/captain/captain.dtoOut";
-import {TeammateDtoOut} from "../features/teammate/teammate.dtoOut";
+import { CaptainDtoOut } from "../features/captain/captain.dtoOut";
+import { TeammateDtoOut } from "../features/teammate/teammate.dtoOut";
+import { UserToRegisterIn } from "../features/user/toRegister.dtoIn";
 
 // Mock of UserRepository
 jest.mock("../features/user/user.repository", () => {
@@ -19,7 +20,7 @@ jest.mock("../features/user/user.repository", () => {
   };
 });
 
-const mockedUserRepo = jest.mocked(UserRepository, {shallow: true});
+const mockedUserRepo = jest.mocked(UserRepository, { shallow: true });
 
 describe("check login details", () => {
   // Initialize userService and UserRepository
@@ -32,20 +33,96 @@ describe("check login details", () => {
   });
 
   it("return a captain", async () => {
-    expect(await userService.checkUserLoginDetails({login: "lavigne", password: "lavigne"})).toBeInstanceOf(
-      CaptainDtoOut
-    );
+    expect(
+      await userService.checkUserLoginDetails({
+        login: "lavigne",
+        password: "lavigne",
+      })
+    ).toBeInstanceOf(CaptainDtoOut);
   });
 
   it("return a team mate", async () => {
-    expect(await userService.checkUserLoginDetails({login: "dugrand", password: "dugrand"})).toBeInstanceOf(
-      TeammateDtoOut
-    );
+    expect(
+      await userService.checkUserLoginDetails({
+        login: "dugrand",
+        password: "dugrand",
+      })
+    ).toBeInstanceOf(TeammateDtoOut);
   });
 
   it("login is good but not password", () => {
-    expect(userService.checkUserLoginDetails({login: "lavigne", password: "wrongpassword"})).rejects.toThrowError(
-      "Bad authentification details"
-    );
+    expect(
+      userService.checkUserLoginDetails({
+        login: "lavigne",
+        password: "wrongpassword",
+      })
+    ).rejects.toThrowError("Bad authentification details");
+  });
+
+  describe("UserService", () => {
+    let userService: UserService;
+    let userRepository: UserRepository;
+
+    beforeEach(() => {
+      userRepository = new UserRepository();
+      userService = new UserService(userRepository);
+    });
+
+    describe("resetPassword", () => {
+      it("should reset the password for a user", async () => {
+        //Arrange
+        const user = new UserToRegisterIn(
+          "Arthur",
+          "Lupine",
+          "arthurlupine@example.com",
+          "password"
+        );
+        const expectedResponse = true;
+
+        //Act
+        const response = await userService.resetPassword(user);
+
+        //Assert
+        expect(response).toEqual(expectedResponse);
+      });
+      it("should thro an error if the user data is incorrect", async () => {
+        //Arrange
+        const user = new UserToRegisterIn("", "", "", "");
+
+        //Act and Assert
+        await expect(userService.resetPassword(user)).rejects.toThrowError(
+          "Incorrect data"
+        );
+      });
+
+      it("should throw an error if the user data is missing", async () => {
+        //Arrange
+        const user = new UserToRegisterIn("", "", "", "password");
+
+        //Act and Assert
+        await expect(userService.resetPassword(user)).rejects.toThrowError(
+          "Missing registration information(s)"
+        );
+      });
+
+      it("should throw an error if the password reset failed", async () => {
+        //Arrange
+        const user = new UserToRegisterIn(
+          "Arthur",
+          "Lupine",
+          "arthurlupine@exemple.com",
+          "password"
+        );
+
+        jest
+          .spyOn(userRepository, "resetPassword")
+          .mockRejectedValueOnce(new Error("Password reset failed"));
+
+        //Act and Assert
+        await expect(userService.resetPassword(user)).rejects.toThrowError(
+          "Password reset failed"
+        );
+      });
+    });
   });
 });
