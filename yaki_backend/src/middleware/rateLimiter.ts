57c74cf10ff0515ce650e1f2c7cfeca37dcad73b
profotoce59 <romain.vanhee@gmail.com>
2023-09-07 14:44:50 +0200

    chore/middleware_mobile

--- yaki_backend/src/middleware/rateLimiter.ts
+++ yaki_backend/src/middleware/rateLimiter.ts
@@ -1,15 +1,15 @@
 const rateLimiter = require("express-rate-limit");
- 
+const envMode = process.env.NODE_ENV; 
 export const limiter = rateLimiter({
-    max: 10,
+    max: (envMode === 'karate') ? 300: 10,
     windowMS: 10000, // 10 seconds
     message: "You can't make any more requests at the moment. Try again later",
 });
  
 export const signInLimiter = rateLimiter({
-    max: 3,
+    max: (envMode === 'karate') ? 300: 3,
     windowMS: 10000, //10 seconds
     message: "Too many sign-in attempts. Try again later."
 })
- 
+//if we are in a karate env we set the limit to 1000 to allow karate testing to spam requests.
   
