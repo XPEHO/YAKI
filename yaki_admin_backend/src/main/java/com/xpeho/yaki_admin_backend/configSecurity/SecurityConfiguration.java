7a2557af60ffff4413ff7367409ab6903ebb6f08
Romain Vanhée <71013671+profotoce59@users.noreply.github.com>
2023-09-18 11:52:41 +0200

    fix(revert_prometheus_admin): revert change as it brokes UAT for unexpected reason (#802)

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/SecurityConfiguration.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/configSecurity/SecurityConfiguration.java
@@ -39,7 +39,6 @@ public class SecurityConfiguration {
                 .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
-                                "/actuator/prometheus",
                                 "/login/**",
                                 "/swagger-ui/**",
                                 "/swagger-resources/**",
