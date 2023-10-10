7709dc6b4992f204b91d9c19ff14406aba62a2fc
Decambray Samir <92584130+SDecambray@users.noreply.github.com>
2023-10-10 13:44:08 +0200

    fix(regexName): add 'ç' and '-' to the regex (#874)

--- yaki_mobile/lib/presentation/ui/registration/form_functionality.dart
+++ yaki_mobile/lib/presentation/ui/registration/form_functionality.dart
@@ -2,7 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 
 // Validators for each input
 String? nameValidator(String? value) {
-  final nameRegex = RegExp(r"^[A-Za-z ]+$");
+  final nameRegex = RegExp(r"^[A-Za-z ç-]+$");
   if (value == null || value.isEmpty) {
     return tr('registrationInputNameError1');
   }
