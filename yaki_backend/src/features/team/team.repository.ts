a238d0567dfd0b61ee3405a44f4f164925c28de5
Decambray Samir <92584130+SDecambray@users.noreply.github.com>
2023-10-10 15:03:29 +0200

    fix(queryTeamMates): split query & create service (#875)

--- yaki_backend/src/features/team/team.repository.ts
+++ yaki_backend/src/features/team/team.repository.ts
@@ -1,6 +1,6 @@
-import {Client, QueryResult} from "pg";
-import {TeamByCaptDtoIn} from "./teamByCaptainId.dtoIn";
-import {TeamDtoIn} from "./team.dtoIn";
+import { Client, QueryResult } from 'pg';
+import { TeamByCaptDtoIn } from './teamByCaptainId.dtoIn';
+import { TeamDtoIn } from './team.dtoIn';
 
 export class TeamRepository {
   getTeamByCaptainId = async (captainId: number) => {
@@ -65,7 +65,45 @@ export class TeamRepository {
 
       const teamListToReturn: TeamDtoIn[] = [];
       for (let team of result.rows) {
-        teamListToReturn.push(new TeamDtoIn(team.team_id, team.team_name, team.team_actif_flag));
+        teamListToReturn.push(
+          new TeamDtoIn(team.team_id, team.team_name, team.team_actif_flag)
+        );
+      }
+      return teamListToReturn;
+    } finally {
+      client.end();
+    }
+  };
+
+  /**
+   * This function returns the list of teams that a team mate is in
+   * @param user_id the id of the team mate
+   * @returns a list of teams that the team mate is in
+   */
+  getTeamByUserId = async (userId: number) => {
+    const client = new Client({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: Number(process.env.DB_PORT),
+    });
+    const query = `
+
+  -- Get user teams
+  SELECT distinct team_id
+  FROM team
+  JOIN teammate on (teammate_team_id = team_id)
+  JOIN public.user on (teammate_user_id = user_id AND user_id= $1) `;
+    client.connect();
+    try {
+      const result = await client.query(query, [userId]);
+
+      const teamListToReturn: TeamDtoIn[] = [];
+      for (let team of result.rows) {
+        teamListToReturn.push(
+          new TeamDtoIn(team.team_id, team.team_name, team.team_actif_flag)
+        );
       }
       return teamListToReturn;
     } finally {
