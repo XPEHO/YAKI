157ee2c466189ae314d5b9f6b4d4130bab96d759
SamirD <samir.decambray@gmail.com>
2023-10-18 11:17:12 +0200

    feat(SplashScreen): dart format

--- yaki_mobile/lib/presentation/features/splash_screen/splash_screen.dart
+++ yaki_mobile/lib/presentation/features/splash_screen/splash_screen.dart
@@ -6,7 +6,6 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
