221960a7e9da4474047adf9279ae8890ac288d9b
laurent <magrez0laurent@gmail.com>
2023-10-19 15:09:50 +0200

    refactor(signin_email): change sign_in email style

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/listeners/RegistrationListener.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/listeners/RegistrationListener.java
@@ -25,8 +25,43 @@ public class RegistrationListener extends EmailListener<OnRegistrationCompleteEv
         String message = "Please follow the link below to verify your email address." +
                 " If your email address is not verified in 24 hours, your account will be deleted.";
         String subject = "Confirm your Yaki account";
-        String html = "<html><body>" + message + "<br><a href=" + apiUrl + confirmationUrl + ">Confirm Registration</a></body></html>";
+        String html = registrationEmail(apiUrl, confirmationUrl);
         String customId = "Register";
         this.setSenderEmail(user.getEmail(), subject,html,customId);
+    }
+
+
+   private String registrationEmail(String apiUrl, String confirmationUrl) {
+        String emailPart1= """
+                    <div
+                        style="max-width: 560px; padding: 20px; background: #ffffff; border-radius: 5px; margin: 40px auto; font-family: Open Sans,Helvetica,Arial; font-size: 15px; color: #666;">
+                        <div style="color: #444444; font-weight: normal;">
+                            <div style="text-align: center; font-weight: 600; font-size: 26px; padding: 10px 0; border-bottom: solid 3px #eeeeee;">YAKI</div>
+                            <div style="clear: both;"> </div>
+                        </div>
+                        <div style="padding: 0 30px 30px 30px; border-bottom: 3px solid #eeeeee;">
+                            <div style="padding: 30px 0; font-size: 24px; text-align: center; line-height: 40px;">Thank you for signing up!<span
+                       style="display: block;">Please click the following link to activate your account.</span><span style="display: block;">If your
+                       email address is not verified in 24 hours, your account will be deleted.</span></div>
+                            <div style="padding: 10px 0 50px 0; text-align: center;"><a
+                               style="background: #555555; color: #fff; padding: 12px 30px; text-decoration: none; border-radius: 3px; letter-spacing: 0.3px;"
+                       href=
+                """;
+
+            String url = apiUrl + confirmationUrl;
+
+            String emailPart2 = """
+                    >Activate your Account</a></div>
+                            <div style="padding: 15px; background: #eee; border-radius: 3px; text-align: center;">Need help? <a
+                               style="color: #3ba1da; text-decoration: none;" href="mailto:[yaki@xpeho.fr](mailto:yaki@xpeho.fr)">contact us</a> today.</div>
+                        </div>
+                        <div style="color: #999; padding: 20px 30px;">
+                            <div>Thank you!</div>
+                            <div>The <a style="color: #3ba1da; text-decoration: none;" href="https://yaki.xpeho.fr/">YAKI</a> Team</div>
+                        </div>
+                    </div>
+                    """;
+
+            return emailPart1 + url + emailPart2;
     }
 }
