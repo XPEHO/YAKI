6cd80e420686dd90b94c12c3091723734003c619
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-10-20 14:31:48 +0200

    feat(profil-page): remplace padding by sizedbox

--- yaki_mobile/lib/presentation/features/profile/profile.dart
+++ yaki_mobile/lib/presentation/features/profile/profile.dart
@@ -27,103 +27,107 @@ class Profile extends ConsumerWidget {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: Alignment.bottomRight,
-              children: [
-                SizedBox(
-                  height: 160,
-                  width: 160,
-                  child: SvgPicture.asset(
-                    'assets/images/avatar-men.svg',
-                  ),
-                ),
-                Positioned(
-                  bottom: 8,
-                  right: 8,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFFF936B),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                        showModalBottomSheet(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return const AvatarModal();
-                          },
-                        );
-                      },
-                      icon: SvgPicture.asset(
-                        'assets/images/edit.svg',
-                        height: 20,
-                        width: 20,
-                      ),
-                      label: const Text(''),
-                      style: ElevatedButton.styleFrom(
-                        shadowColor: Colors.transparent,
-                      ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: Alignment.bottomRight,
+                children: [
+                  SizedBox(
+                    height: 160,
+                    width: 160,
+                    child: SvgPicture.asset(
+                      'assets/images/avatar-men.svg',
                     ),
                   ),
+                  Positioned(
+                    bottom: 8,
+                    right: 8,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFF936B),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return const AvatarModal();
+                            },
+                          );
+                        },
+                        icon: SvgPicture.asset(
+                          'assets/images/edit.svg',
+                          height: 20,
+                          width: 20,
+                        ),
+                        label: const Text(''),
+                        style: ElevatedButton.styleFrom(
+                          shadowColor: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 16,
+                child: InputText(
+                  type: InputTextType.firstname,
+                  label: tr('inputLabelFirstName'),
+                  controller:
+                      TextEditingController(text: user?.firstName ?? ''),
                 ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
-              child: InputText(
-                type: InputTextType.firstname,
-                label: tr('inputLabelFirstName'),
-                controller: TextEditingController(text: user?.firstName ?? ''),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 4),
-              child: InputText(
-                type: InputTextType.lastname,
-                label: tr('inputLabelLastName'),
-                controller: TextEditingController(text: user?.lastName ?? ''),
+              SizedBox(
+                height: 4,
+                child: InputText(
+                  type: InputTextType.lastname,
+                  label: tr('inputLabelLastName'),
+                  controller: TextEditingController(text: user?.lastName ?? ''),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 4),
-              child: InputText(
-                type: InputTextType.email,
-                label: 'Email',
-                controller: TextEditingController(text: user?.email ?? ''),
+              SizedBox(
+                height: 4,
+                child: InputText(
+                  type: InputTextType.email,
+                  label: 'Email',
+                  controller: TextEditingController(text: user?.email ?? ''),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 4),
-              child: InputText(
-                type: InputTextType.password,
-                label: tr('inputPassword'),
-                controller: TextEditingController(),
+              SizedBox(
+                height: 4,
+                child: InputText(
+                  type: InputTextType.password,
+                  label: tr('inputPassword'),
+                  controller: TextEditingController(),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 4),
-              child: Button.secondary(
-                text: tr('changePassword'),
-                onPressed: () {
-                  context.go('/changePassword');
-                },
+              SizedBox(
+                height: 4,
+                child: Button.secondary(
+                  text: tr('changePassword'),
+                  onPressed: () {
+                    context.go('/changePassword');
+                  },
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 4),
-              child: Button.tertiary(
-                text: tr('logOutButton'),
-                onPressed: () {
-                  context.go('/');
-                },
+              SizedBox(
+                height: 4,
+                child: Button.tertiary(
+                  text: tr('logOutButton'),
+                  onPressed: () {
+                    context.go('/');
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
