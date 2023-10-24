b5b636c5e67dcf03f55a9bb458cce6c06cab3298
Priscillia Ammeux <96787811+PriscilliaAmmeux@users.noreply.github.com>
2023-10-24 16:52:46 +0200

    Fix/fetch profil page (#948)
    * fix(fetch-profil-page): debug print
    
    * fix(fetch-profil-page): fix fetch info on profil page

--- yaki_mobile/lib/presentation/features/profile/profile.dart
+++ yaki_mobile/lib/presentation/features/profile/profile.dart
@@ -14,7 +14,8 @@ class Profile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final LoggedUser? user = ref.watch(loginRepositoryProvider).loggedUser;
+    final LoggedUser? user = ref.read(loginRepositoryProvider).loggedUser;
+    debugPrint(user.toString());
 
     void onLogout({required Function goToAuthentication}) {
       goToAuthentication();
