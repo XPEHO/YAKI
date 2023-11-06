e810747429efd5ab60fd81af3de795f975ec57a5
Decambray Samir <92584130+SDecambray@users.noreply.github.com>
2023-11-06 16:28:26 +0100

    Feat/image picker (#1010)
    * feat(ImagePicker): install image picker
    
    * feat(ImagePicker): add the permission in ios
    
    * feat(ImagePicker): add camera photo to the button
    
    * feat(ImagePicker): package image and path-provider
    
    * feat(ImagePicker): set function and display picture
    
    * feat(ImagePicker):  add translation

--- yaki_mobile/lib/presentation/features/profile/view/avatar_modal.dart
+++ yaki_mobile/lib/presentation/features/profile/view/avatar_modal.dart
@@ -1,17 +1,56 @@
+import 'dart:io';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:yaki/domain/entities/logged_user.dart';
 import 'package:yaki/presentation/features/shared/sized_circle_avatar.dart';
 import 'package:yaki/presentation/state/providers/login_provider.dart';
 import 'package:yaki_ui/yaki_ui.dart';
+import 'package:image/image.dart' as img;
 
-class AvatarModal extends ConsumerWidget {
+class AvatarModal extends ConsumerStatefulWidget {
   const AvatarModal({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  AvatarModalState createState() => AvatarModalState();
+}
+
+class AvatarModalState extends ConsumerState<AvatarModal> {
+  File? _imageFile;
+
+  @override
+  Widget build(BuildContext context) {
     final LoggedUser? user = ref.watch(loginRepositoryProvider).loggedUser;
+
+    Future<void> pickImage(ImageSource source) async {
+      final pickedFile = await ImagePicker().pickImage(source: source);
+      if (pickedFile != null) {
+        final originalImage = img.decodeImage(await pickedFile.readAsBytes());
+        final resizedImage = img.copyResize(
+          originalImage!,
+          width: 160,
+          height: 160,
+        );
+
+        final directory = await getApplicationDocumentsDirectory();
+        final path = directory.path;
+        final File resizedImageFile = File('$path/resized_image.jpg')
+          ..writeAsBytesSync(img.encodeJpg(resizedImage));
+
+        setState(() {
+          _imageFile = resizedImageFile;
+        });
+      }
+    }
+
+    Future<void> uploadImage() async {
+      // Upload the image file to the server
+      // ...
+      // Once the upload is complete, close the modal bottom sheet
+      Navigator.of(context).pop();
+    }
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -122,39 +161,29 @@ class AvatarModal extends ConsumerWidget {
                 children: [
                   Button.secondary(
                     text: tr('takePicture'),
-                    onPressed: () {
-                      // This is a modal bottom sheet. This need to be delete when the method will be implemented
-                      showModalBottomSheet(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return const SizedBox(
-                            height: 200,
-                            child: Center(
-                              child: Text('Coming soon'),
-                            ),
-                          );
-                        },
-                      );
-                    },
+                    onPressed: () => pickImage(ImageSource.camera),
                   ),
                   const SizedBox(height: 10),
                   Button.secondary(
                     text: tr('imgGallery'),
-                    onPressed: () {
-                      // This is a modal bottom sheet. This need to be delete when the method will be implemented
-                      showModalBottomSheet(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return const SizedBox(
-                            height: 200,
-                            child: Center(
-                              child: Text('Coming soon'),
-                            ),
-                          );
-                        },
-                      );
-                    },
+                    onPressed: () => pickImage(ImageSource.gallery),
                   ),
+                  const SizedBox(height: 10),
+                  if (_imageFile != null)
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(
+                        100.0,
+                      ), // adjust the radius as needed
+                      child: Image.file(
+                        _imageFile!,
+                      ),
+                    ),
+                  const SizedBox(height: 20),
+                  if (_imageFile != null)
+                    Button.secondary(
+                      text: tr('upload'),
+                      onPressed: uploadImage,
+                    ),
                 ],
               ),
             ],
