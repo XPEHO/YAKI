2bd6faace080aaae30927c5509a01de478bf1380
SamirD <samir.decambray@gmail.com>
2023-11-10 12:51:01 +0100

    feat(Error): add profil and change redirection

--- yaki_mobile/lib/presentation/features/profile/profile.dart
+++ yaki_mobile/lib/presentation/features/profile/profile.dart
@@ -9,6 +9,7 @@ import 'package:yaki/presentation/displaydata/avatar_enum.dart';
 import 'package:yaki/presentation/features/profile/view/avatar_modal.dart';
 import 'package:yaki/presentation/features/shared/feedback_user.dart';
 import 'package:yaki/presentation/state/providers/avatar_provider.dart';
+import 'package:yaki/presentation/state/providers/declaration_provider.dart';
 import 'package:yaki/presentation/state/providers/user_provider.dart';
 import 'package:yaki_ui/yaki_ui.dart';
 
@@ -31,14 +32,29 @@ class Profile extends ConsumerWidget {
       onLogout(goToAuthentication: goToAuthentication);
     }
 
+    void onReturn({
+      required Function goToTeamsDeclarationSummary,
+      required Function goToTeamSelectionPage,
+    }) async {
+      final bool isDeclared =
+          await ref.read(declarationProvider.notifier).getLatestDeclaration();
+      if (isDeclared) {
+        goToTeamsDeclarationSummary();
+      } else {
+        goToTeamSelectionPage();
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
         leading: IconButton(
-          onPressed: () {
-            context.go('/teams-declaration-summary');
-          },
+          onPressed: () => onReturn(
+            goToTeamsDeclarationSummary: () =>
+                context.go('/teams-declaration-summary'),
+            goToTeamSelectionPage: () => context.go('/team-selection'),
+          ),
           icon: const Icon(Icons.arrow_back),
         ),
       ),
