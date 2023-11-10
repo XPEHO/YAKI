006278db5548e9502533de6a43f402d7ba436a65
SamirD <samir.decambray@gmail.com>
2023-11-10 17:05:16 +0100

    feat(App_bar_date): add conditionnal rendering

--- yaki_mobile/lib/presentation/features/shared/app_bar_date.dart
+++ yaki_mobile/lib/presentation/features/shared/app_bar_date.dart
@@ -1,29 +1,56 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:yaki/presentation/state/providers/declaration_provider.dart';
 import 'package:yaki/presentation/styles/text_style.dart';
 
-class AppBarWithDate extends StatelessWidget implements PreferredSizeWidget {
+class AppBarWithDate extends ConsumerWidget implements PreferredSizeWidget {
   final List<Widget>? actions;
 
-  const AppBarWithDate({super.key, this.actions});
+  const AppBarWithDate({Key? key, this.actions}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final toDayDate = DateFormat('d MMMM y').format(DateTime.now());
+    final currentRoute = ModalRoute.of(context)?.settings.name;
+    final showBackButton = currentRoute == 'team-selection';
 
-    return AppBar(
-      centerTitle: false,
-      automaticallyImplyLeading: false,
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      title: Padding(
-        padding: const EdgeInsets.only(left: 16),
-        child: Text(
-          toDayDate,
-          style: textStylePageDate(),
-        ),
-      ),
-      actions: actions,
+    Future<bool> getIsDeclared() async {
+      return await ref
+          .read(declarationProvider.notifier)
+          .getLatestDeclaration();
+    }
+
+    return FutureBuilder<bool>(
+      future: getIsDeclared(),
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator(); // or some other widget while waiting for data
+        } else {
+          final isDeclared = snapshot.data ?? false;
+          return AppBar(
+            centerTitle: false,
+            automaticallyImplyLeading: showBackButton && isDeclared,
+            leading: showBackButton && isDeclared
+                ? IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () => context.go('/teams-declaration-summary'),
+                  )
+                : null,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            title: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text(
+                toDayDate,
+                style: textStylePageDate(),
+              ),
+            ),
+            actions: actions,
+          );
+        }
+      },
     );
   }
 
