dcaeec234d0c12e4d4db501ed087376f0609744d
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2023-11-14 17:14:57 +0100

    fix(email-support): support email

--- yaki_mobile/lib/presentation/features/shared/feedback_user.dart
+++ yaki_mobile/lib/presentation/features/shared/feedback_user.dart
@@ -1,9 +1,19 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:yaki/presentation/styles/text_style.dart';
 
 class FeedbackUser extends StatelessWidget {
   const FeedbackUser({Key? key}) : super(key: key);
+
+  Future<void> _launchURL() async {
+    const url = 'mailto:yaki@xpeho.fr?subject=Feedback&body=';
+    if (await canLaunchUrl(url as Uri)) {
+      await launchUrl(url as Uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +23,7 @@ class FeedbackUser extends StatelessWidget {
       children: [
         GestureDetector(
           onTap: () {
+            _launchURL();
             showModalBottomSheet(
               context: context,
               builder: (BuildContext context) {
