9b47c260cab6c34c755df69705d0aff9a5576414
Priscillia Ammeux <96787811+PriscilliaAmmeux@users.noreply.github.com>
2023-11-20 11:30:52 +0100

    fix(button-feedback): configuration android manifest (#1086)
    * fix(button-feedback): configuration android manifest
    
    * fix(button-feedback): configuration android manifest

--- yaki_mobile/lib/presentation/features/shared/feedback_user.dart
+++ yaki_mobile/lib/presentation/features/shared/feedback_user.dart
@@ -12,26 +12,21 @@ class FeedbackUser extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        GestureDetector(
+        InkWell(
           onTap: () async {
-            const String url = 'mailto:yaki@xpeho.fr';
-            final Uri uri = Uri.parse(url);
+            final Uri uri = Uri.parse('mailto:yaki@xpeho.fr');
             if (await canLaunchUrl(uri)) {
               if (!await launchUrl(
                 uri,
                 mode: LaunchMode.externalApplication,
               )) {
-                throw Exception('Could not launch $url');
+                throw Exception('Could not launch $uri');
               }
             }
           },
-          child: Row(
-            children: [
-              Text(
-                tr('feedback'),
-                style: textFeedback(),
-              ),
-            ],
+          child: Text(
+            tr('feedback'),
+            style: textFeedback(),
           ),
         ),
       ],
