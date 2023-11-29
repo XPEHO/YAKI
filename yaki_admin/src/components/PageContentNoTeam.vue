f6dba32622be59dd550329317f57dd13815f0b4d
laurent <magrez0laurent@gmail.com>
2023-11-29 18:02:40 +0100

    feat(info_no_team): change captain page content to display a no team message

---
+++ yaki_admin/src/components/PageContentNoTeam.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import Tutorial from "@/assets/images/tuto_team_creation.png";
+import avatar from "@/assets/images/avatar_2.png";
+import {useTeamStore} from "@/stores/teamStore";
+import {useRoute} from "vue-router";
+
+const teamStore = useTeamStore();
+
+const route = useRoute();
+const teamParamState = route.params.state;
+</script>
+
+<template>
+  <section class="no-team__container">
+    <div
+      v-if="teamParamState === 'deleted'"
+      class="no-team-in-list">
+      <p class="p_main p_common">The team : {{ teamStore.getTeamDeleted.teamName }}</p>
+      <figure class="deleted_team__figure">
+        <img
+          :src="avatar"
+          alt="" />
+      </figure>
+      <p class="p_secondary p_common margin-top-2">was correctly deleted.</p>
+
+      <div
+        v-if="teamStore.getTeamList.length === 0"
+        class="margin-top-3">
+        <p class="p_secondary p_common">You have no team left</p>
+        <p class="p_secondary p_common margin-top-1">
+          You can create a new team with the <span>create team</span> option
+        </p>
+      </div>
+    </div>
+
+    <div
+      v-else
+      class="no-team-in-list">
+      <p class="p_main p_common">You dont have any team</p>
+      <p class="p_secondary p_common margin-top-3">Teams can be created with the <span>create team</span> option</p>
+      <figure class="no_team__figure">
+        <img
+          :src="Tutorial"
+          alt="" />
+      </figure>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.margin-top-3 {
+  margin-top: 3rem;
+}
+
+.margin-top-2 {
+  margin-top: 2rem;
+}
+
+.margin-top-1 {
+  margin-top: 1rem;
+}
+.no-team__container {
+  background-color: #ecf0f5;
+
+  padding-block-start: 10rem;
+
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.no-team-in-list {
+  max-width: 30rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.p_common {
+  color: #7d818c;
+  text-align: center;
+}
+.p_main {
+  font-size: 1.4rem;
+  font-weight: 800;
+  line-height: 120%;
+}
+
+.p_secondary {
+  font-size: 1.2rem;
+  font-weight: 500;
+
+  span {
+    font-weight: 700;
+  }
+}
+
+.deleted_team__figure {
+  padding-block-start: 1rem;
+  max-width: 10rem;
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+}
+
+.no_team__figure {
+  max-width: 25rem;
+
+  img {
+    width: 100%;
+    object-fit: cover;
+    filter: grayscale(20%);
+  }
+}
+</style>
--- yaki_admin/src/stores/teamStore.ts
+++ yaki_admin/src/stores/teamStore.ts
@@ -7,14 +7,20 @@ import {useTeammateStore} from "@/stores/teammateStore";
 
 interface State {
   teamList: TeamType[];
+  isTeamListSetOnLoggin: boolean;
   teamSelected: TeamType;
+  teamDeleted: TeamType;
   captainIdToBeAssign: number | null;
 }
 
 export const useTeamStore = defineStore("teamStore", {
   state: () => ({
     teamList: [] as TeamType[],
+    // attribute false when a user log in, and set to true when the teamList is fetched for the very first time.
+    // This way the router will not trigger fetch everytime the user navigate to the captain page.
+    isTeamListSetOnLoggin: false as boolean,
     teamSelected: {} as TeamType,
+    teamDeleted: {} as TeamType,
 
     // DEPRECIATED WITH BASTI CHANGE ? ------------------------------------
     // id of the captain that will be assigned to a team
@@ -22,16 +28,23 @@ export const useTeamStore = defineStore("teamStore", {
   }),
   getters: {
     getTeamList: (state: State) => state.teamList,
+    getIsTeamListSetOnLoggin: (state: State) => state.isTeamListSetOnLoggin,
     getTeamSelected: (state: State) => state.teamSelected,
+    getTeamDeleted: (state: State) => state.teamDeleted,
 
     // DEPRECIATED WITH BASTI CHANGE ? ------------------------------------
     getCaptainIdToBeAssign: (state: State) => state.captainIdToBeAssign,
   },
   actions: {
+    setIsTeamListSetOnLoggin(isTeamListSet: boolean): void {
+      this.isTeamListSetOnLoggin = isTeamListSet;
+    },
     setTeamSelected(team: TeamType): void {
       this.teamSelected = team;
     },
-
+    setTeamDeleted(team: TeamType): void {
+      this.teamDeleted = team;
+    },
     isSameTeamId(teamId: number): boolean {
       if (this.getTeamSelected.id === teamId) {
         return true;
@@ -63,17 +76,25 @@ export const useTeamStore = defineStore("teamStore", {
       );
       const teams = await Promise.all(promises);
       this.teamList = [...teams.flat()];
+
+      // change attribute value to true after the first fetch
+      if (this.getIsTeamListSetOnLoggin === false) {
+        this.setIsTeamListSetOnLoggin(true);
+      }
     },
 
     // add a selected user to a team
     async addUserToTeam(userId: number): Promise<void> {
+      const teammateStore = useTeammateStore();
       const data = {teamId: this.getTeamSelected.id, userId: userId};
       await teammateService.createTeammate(data);
+      await teammateStore.setListOfTeammatesWithinTeam(this.getTeamSelected.id);
     },
 
     /**
      * Create a team, assign to the connected captain and his customer
      * @param teamName name of the team
+     * @returns created team: TeamType
      */
     async createTeam(teamName: string): Promise<TeamType> {
       const selectedRoleStore = useSelectedRoleStore();
@@ -92,23 +113,25 @@ export const useTeamStore = defineStore("teamStore", {
      * @param teamName New team name. null if the team name is not being updated.
      * @param customerId id of the customer that the team belongs to.
      *
-     * @returns Resolves when the team information has been updated in the database.
+     * @returns updated team: TeamType.
      */
     async updateTeam(
       teamID: number,
       cptId: number | null,
       teamName: string | null,
       customerId: number | null
-    ): Promise<void> {
-      await teamService.updateTeam(teamID, cptId, teamName, customerId);
+    ): Promise<TeamType> {
+      return await teamService.updateTeam(teamID, cptId, teamName, customerId);
     },
 
     /**
      * Delete a team by its id
-     * @param teamId
+     * @param deletedTeam: TeamType
      */
-    async deleteTeam(teamId: number): Promise<void> {
-      await teamService.deleteTeam(teamId);
+    async deleteTeam(teamId: number): Promise<TeamType> {
+      const deletedTeam = await teamService.deleteTeam(teamId);
+      this.setTeamDeleted(deletedTeam);
+      return this.getTeamDeleted;
     },
 
     // DEPRECIATED WITH BASTI CHANGE ? ---------------------------------------------------------------
