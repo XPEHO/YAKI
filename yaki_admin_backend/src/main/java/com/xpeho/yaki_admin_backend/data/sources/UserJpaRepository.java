2061b2b58b7760074b1e076dc56a1b53b87b3ffb
Priscillia Ammeux <96787811+PriscilliaAmmeux@users.noreply.github.com>
2024-01-30 09:57:34 +0100

    fix(fetch-user): add extension page with new route for to have every … (#1260)
    * fix(fetch-user): add extension page with new route for to have every user fetching
    
    * fix(fetch-user): add extension page with new route for to have every user fetching
    
    * fix(fetch-user): add extension page with new route for to have every user fetching

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/sources/UserJpaRepository.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/sources/UserJpaRepository.java
@@ -3,15 +3,13 @@ package com.xpeho.yaki_admin_backend.data.sources;
 import com.xpeho.yaki_admin_backend.data.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import java.util.List;
 import java.util.Optional;
 
 public interface UserJpaRepository extends JpaRepository<UserModel, Integer> {
     Optional<UserModel> findByLogin(String login);
 
-    List<UserModel> findByUserIdBetween(int idStart,int idEnd);
-
+    List<UserModel> findByUserIdBetween(int idStart, int idEnd);
 
     boolean existsByEmail(String email);
 }
--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
@@ -6,6 +6,8 @@ import com.xpeho.yaki_admin_backend.domain.entities.UserEntityIn;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntityWithID;
 import com.xpeho.yaki_admin_backend.domain.services.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,16 +22,27 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @PostMapping
     public UserEntity createUser(@RequestBody UserEntityIn userEntity) {
         return userService.save(userEntity);
     }
+
     @GetMapping("/inRange")
     public List<UserEntityWithID> findUserByIdRange(@RequestParam int idStart,
                                                     @RequestParam int idEnd) {
         return userService.findUserByIdRange(idStart, idEnd);
     }
-    
+
+    @GetMapping()
+    public List<UserEntityWithID> findAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return userService.findAllUsers(pageable).getContent();
+    }
+
+
     @DeleteMapping("{id}")
     public UserEntity deleteUser(@PathVariable int id) {
         return userService.deleteById(id);
