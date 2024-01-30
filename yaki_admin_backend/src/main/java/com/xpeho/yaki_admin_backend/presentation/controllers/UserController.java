68f047aa31050f1f099ecf6aff69512692f329d6
Priscillia Ammeux <96787811+PriscilliaAmmeux@users.noreply.github.com>
2024-01-30 11:53:14 +0100

    fix(get-total-pages): add the possibility to have the total page for … (#1264)
    * fix(get-total-pages): add the possibility to have the total page for fetch all users in findAllUsers
    
    * fix(get-total-pages): add the possibility to have the total page for fetch all users in findAllUsers

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/presentation/controllers/UserController.java
@@ -6,11 +6,16 @@ import com.xpeho.yaki_admin_backend.domain.entities.UserEntityIn;
 import com.xpeho.yaki_admin_backend.domain.entities.UserEntityWithID;
 import com.xpeho.yaki_admin_backend.domain.services.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -34,12 +39,30 @@ public class UserController {
         return userService.findUserByIdRange(idStart, idEnd);
     }
 
+    // Define a GET mapping for the root path. This method will be invoked when a GET request is made to the root path.
     @GetMapping()
-    public List<UserEntityWithID> findAllUsers(
+    public ResponseEntity<Map<String, Object>> findAllUsers(
+            // Request parameters for the page number and size. If not provided, they default to 0 and 10 respectively.
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
+
+        // Create a Pageable object with the provided page number and size. This is used to add pagination information to the database query.
         Pageable pageable = PageRequest.of(page, size);
-        return userService.findAllUsers(pageable).getContent();
+
+        // Call the userService to get a Page of UserEntityWithID. The Page includes the data for the requested page, as well as pagination information.
+        Page<UserEntityWithID> userPage = userService.findAllUsers(pageable);
+
+        // Create a map to hold the response data.
+        Map<String, Object> response = new HashMap<>();
+
+        // Add the list of users to the response map. The getContent() method is used to get the list of UserEntityWithID from the Page.
+        response.put("users", userPage.getContent());
+
+        // Add the total number of pages to the response map. The getTotalPages() method is used to get the total number of pages from the Page.
+        response.put("totalPages", userPage.getTotalPages());
+
+        // Return the response map as the body of a ResponseEntity. The status is set to OK to indicate that the request was successful.
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
