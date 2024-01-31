d9de02163efc086b46cec74783cd64c528b012a6
Laurent <95299697+LaurentMag@users.noreply.github.com>
2024-01-31 15:36:12 +0100

    fix(profil_acess): add profil access in the page of user without teams (#1278)

--- yaki_mobile/lib/presentation/ui/default/user_default_redirection.dart
+++ yaki_mobile/lib/presentation/ui/default/user_default_redirection.dart
@@ -1,15 +1,35 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:yaki/presentation/features/shared/app_bar_date.dart';
+import 'package:yaki/presentation/features/shared/sized_circle_avatar.dart';
+import 'package:yaki/presentation/state/providers/avatar_provider.dart';
+import 'package:yaki/presentation/state/providers/user_provider.dart';
 import 'package:yaki/presentation/styles/color.dart';
 import 'package:yaki/presentation/styles/text_style.dart';
 import 'package:yaki/presentation/ui/shared/views/circle_avatar_svg.dart';
 
-class UserDefaultRedirection extends StatelessWidget {
+class UserDefaultRedirection extends ConsumerWidget {
   const UserDefaultRedirection({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      appBar: AppBarWithDate(
+        actions: <Widget>[
+          IconButton(
+            icon: const NavIconCircleAvatar(
+              imageSrc: 'assets/images/Account.svg',
+            ),
+            onPressed: () {
+              ref.read(userProvider.notifier).getUser();
+              ref.read(avatarProvider.notifier).getAvatar();
+              context.push('/profile');
+            },
+          ),
+        ],
+      ),
       backgroundColor: AppColors.yakiPrimaryColor,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
