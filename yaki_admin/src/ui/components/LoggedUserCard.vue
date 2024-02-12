2a8e1c9c794cacfeabaaf034ee89017419d7435e
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2024-02-12 14:20:49 +0100

    feat(display-user-sidebar-admin): fetch user with avatar initial

--- yaki_admin/src/ui/components/LoggedUserCard.vue
+++ yaki_admin/src/ui/components/LoggedUserCard.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import avatarIcon from "@/assets/images/avatarLetters.png";
+import InitialAvatar from "@/ui/components/InitialAvatar.vue";
 import { UserWithIdType } from "@/models/userWithId.type";
 import router from "@/router/router";
 import { usersService } from "@/services/users.service";
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
 
 const profile = () => {
   router.push("/dashboard/profile");
 };
 
-let user: UserWithIdType | null = null;
+let user = ref<UserWithIdType | null>(null);
 
 const fetchUser = async () => {
   let storedUser = localStorage.getItem("user");
@@ -23,9 +23,10 @@ const fetchUser = async () => {
   console.log("Token:", token);
 
   try {
-    user = await usersService.getCurrentUser(token);
+    const fetchedUser = await usersService.getCurrentUser(token);
+    user.value = fetchedUser;
 
-    console.log("User:", user); // Log the user
+    console.log("User:", user.value); // Log the user
   } catch (e) {
     console.error("Error fetching user:", e);
   }
@@ -40,14 +41,14 @@ onMounted(fetchUser);
     class="user-card__container logged_user_card selection-cursor"
   >
     <figure class="user-card__avatar">
-      <img
-        :src="avatarIcon"
-        alt="user-card"
+      <InitialAvatar
+        :firstname="user?.firstname"
+        :lastname="user?.lastname"
       />
     </figure>
     <div class="user-card__wrapper-user-infos">
-      <p class="user-card__name-text"></p>
-      <p class="user-card__name_text"></p>
+      <p class="user-card__name-text">{{ user?.firstname }}</p>
+      <p class="user-card__name_text">{{ user?.lastname }}</p>
     </div>
   </article>
 </template>
