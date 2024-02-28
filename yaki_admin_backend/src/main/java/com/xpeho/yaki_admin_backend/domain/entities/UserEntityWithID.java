f1529602552cf263b2d96c37fc865390e5e45655
Priscillia Ammeux <96787811+PriscilliaAmmeux@users.noreply.github.com>
2024-02-28 15:05:12 +0100

    fix(bug-back-admin-sonarque): report bug in sonarqure recommand to Eq… (#1320)
    * fix(bug-back-admin-sonarque): report bug in sonarqure recommand to Equals method should be overridden in records containing array field
    
    * fix(bug-back-admin-sonarque): report bug in sonarqure recommand to Equals method should be overridden in records containing array field

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/domain/entities/UserEntityWithID.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/domain/entities/UserEntityWithID.java
@@ -1,5 +1,8 @@
 package com.xpeho.yaki_admin_backend.domain.entities;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record UserEntityWithID(
         Integer id,
         Integer captainId,
@@ -9,4 +12,39 @@ public record UserEntityWithID(
         String email,
         String avatarReference,
         byte[] avatarBlob) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntityWithID that = (UserEntityWithID) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(captainId, that.captainId) &&
+                Objects.equals(teammateId, that.teammateId) &&
+                Objects.equals(lastname, that.lastname) &&
+                Objects.equals(firstname, that.firstname) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(avatarReference, that.avatarReference) &&
+                Arrays.equals(avatarBlob, that.avatarBlob);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, captainId, teammateId, lastname, firstname, email, avatarReference);
+        result = 31 * result + Arrays.hashCode(avatarBlob);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntityWithID{" +
+                "id=" + id +
+                ", captainId=" + captainId +
+                ", teammateId=" + teammateId +
+                ", lastname='" + lastname + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", email='" + email + '\'' +
+                ", avatarReference='" + avatarReference + '\'' +
+                ", avatarBlob=" + Arrays.toString(avatarBlob) +
+                '}';
+    }
 }
