1aa2bb7afcef413907fff1a7627a1a7a28183c71
Priscillia Ammeux <priscillia.ammeux@gmail.com>
2024-02-28 14:45:03 +0100

    fix(bug-back-admin-sonarque): report bug in sonarqure recommand to Equals method should be overridden in records containing array field

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/domain/entities/TeamLogoEntity.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/domain/entities/TeamLogoEntity.java
@@ -1,4 +1,31 @@
 package com.xpeho.yaki_admin_backend.domain.entities;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record TeamLogoEntity(int teamId, byte[] teamLogoBlob) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TeamLogoEntity that = (TeamLogoEntity) o;
+        return teamId == that.teamId &&
+                Arrays.equals(teamLogoBlob, that.teamLogoBlob);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(teamId);
+        result = 31 * result + Arrays.hashCode(teamLogoBlob);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TeamLogoEntity{" +
+                "teamId=" + teamId +
+                ", teamLogoBlob=" + Arrays.toString(teamLogoBlob) +
+                '}';
+    }
 }
