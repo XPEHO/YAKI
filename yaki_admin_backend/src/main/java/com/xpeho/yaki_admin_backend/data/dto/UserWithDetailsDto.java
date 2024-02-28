45ce39ea952b84aca2f64c9c3c98e7db0f62b5b5
Priscillia Ammeux <96787811+PriscilliaAmmeux@users.noreply.github.com>
2024-02-28 14:10:15 +0100

    fix(bug-sonarqube-back-admin): fix bug in report sonarqube Override equals, hashCode and toString to consider array's content in the method (#1318)

--- yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/dto/UserWithDetailsDto.java
+++ yaki_admin_backend/src/main/java/com/xpeho/yaki_admin_backend/data/dto/UserWithDetailsDto.java
@@ -1,5 +1,8 @@
 package com.xpeho.yaki_admin_backend.data.dto;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record UserWithDetailsDto(
         Integer id,
         Integer captainId,
@@ -8,4 +11,38 @@ public record UserWithDetailsDto(
         String email,
         String avatarReference,
         byte[] avatarBlob) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserWithDetailsDto that = (UserWithDetailsDto) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(captainId, that.captainId) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(avatarReference, that.avatarReference) &&
+                Arrays.equals(avatarBlob, that.avatarBlob);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, captainId, lastName, firstName, email, avatarReference);
+        result = 31 * result + Arrays.hashCode(avatarBlob);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserWithDetailsDto{" +
+                "id=" + id +
+                ", captainId=" + captainId +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                ", avatarReference='" + avatarReference + '\'' +
+                ", avatarBlob=" + Arrays.toString(avatarBlob) +
+                '}';
+    }
 }
