21e87f87f6e871830a1f2d3a6231d1662b422a75
Ryan Debouvries <debouvriesryan@gmail.com>
2024-04-30 11:19:21 +0200

    fix(reload_persist): Make return to login page on reload to prevent role rights issues

--- yaki_admin/src/ui/views/ProfileView.vue
+++ yaki_admin/src/ui/views/ProfileView.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import PageContentHeader from "@/ui/components/PageContentHeader.vue";
 import PageContentLayout from "@/ui/layouts/PageContentLayout.vue";
-import router from "@/router/router";
 import buttonSecondary from "@/ui/components/buttons/ButtonSecondary.vue";
 import LanguageSwitch from "../components/LanguageSwitch.vue";
+import { useAuthStore } from "@/stores/authStore";
 
 const logOff = () => {
-  router.push("/");
+  const authStore = useAuthStore();
+  authStore.logout();
 };
 </script>
 
